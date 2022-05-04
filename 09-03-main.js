89bfc0d94efb2f8e4e1165de6ec855bbdc09f26b
nghiemph95 <nghiemph95@gmail.com>
2022-05-04 14:39:15 +0700

    update

--- 09-03-main.js
+++ 09-03-main.js
@@ -38,20 +38,86 @@ console.log(
 
 /** filter fn */
 function arrFilter(arr, filterCallback) {
-  if (!Array.isArray(arr) || typeof filterCallback !== "function")
-    return undefined;
+  if (!Array.isArray(arr) || typeof filterCallback !== "function") return;
 
   const newArr = [];
 
   for (let i = 0; i < arr.length; i++) {
-    const arrList = arr[i];
-    if (filterCallback(arrList, i)) {
-      newArr.push(arrList);
+    // const arrList = arr[i];
+    if (filterCallback(arr[i])) {
+      newArr.push(arr[i]);
     }
+    // if ((arrList) => arrList > 3)
   }
 
   return newArr;
 }
 
 console.log(arrFilter([1, 2, 3, 4, 5, 6, 7], (x) => x > 3));
-console.log(first);
+
+/** reduce fn */
+const result = [2, 3, 4].reduce(
+  (previousValue, currentValue) => previousValue + currentValue,
+  1
+);
+console.log(result);
+
+// const hasInitialValue = !initialValue;
+// const hasInitialValue = initialValue !== undefined;
+
+// !null --> true
+// !undefined --> true
+
+/** reduce fn - for i */
+function reduceFunc(arr, callBackFn, initialValue) {
+  if (!Array.isArray(arr) || typeof callBackFn !== "function")
+    throw new Error("Invalid parameters");
+
+  if (arr.length === 0) {
+    if (initialValue === undefined) {
+      throw new Error("InitialValue should have when array is empty!");
+    }
+    return initialValue;
+  }
+
+  const hasInitialValue = initialValue !== undefined;
+  const startIndex = hasInitialValue ? 0 : 1;
+  let previousValue = hasInitialValue ? initialValue : arr[0];
+
+  for (let i = startIndex; i < arr.length; i++) {
+    previousValue = callBackFn(previousValue, arr[i], i);
+  }
+  return previousValue;
+}
+
+console.log(
+  reduceFunc(
+    [2, 3, 4],
+    (previousValue, currentValue) => previousValue + currentValue,
+    0
+  )
+);
+
+/** print number */
+function printNumbers() {
+  for (let i = 0; i <= 10; i++) {
+    console.log(i);
+  }
+}
+printNumbers();
+
+function printNumbers() {
+  for (let i = 0; i <= 10; i++) {
+    if (i % 2 === 0) console.log(i);
+  }
+}
+printNumbers();
+
+function printNumbers() {
+  for (let i = 0; i <= 10; i += 2) {
+    console.log(i);
+  }
+}
+printNumbers();
+
+/** find max */
