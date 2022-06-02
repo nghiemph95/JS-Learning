4c3fa9b9ff2af72cdf994e9a96be5b965972cbc8
nghiemph95 <nghiemph95@gmail.com>
2022-06-02 16:32:02 +0700

    i

---
+++ sml/countEmails.js
@@ -0,0 +1,6 @@
+// Email là một chuỗi có chứa ký tự @ ở giữa và không phải ở cuối từ.
+// Email có thể có một số domain như: .com, .vn, .com.vn. Domain sẽ xuất hiện ở cuối từ.
+// Email có định dạng là X@Y. Trong đó X có độ dài ít nhất 3 ký tự, Y có độ dài ít nhất 3 ký tự (ko tính
+// phần domain nêu trên)
+
+export function countEmails(str) {}
---
+++ sml/countEmails.test.js
@@ -0,0 +1,17 @@
+import { countEmails } from "./countEmails";
+
+describe("Test countEmails()", () => {
+  test("should return the number of Email", () => {
+    expect(countEmails("my email should be abc@super.com")).toEqual(1);
+    expect(
+      countEmails(
+        "my email should be easy@frontend.com or easy@frontend.com.vn or easy@frontend.vn"
+      )
+    ).toEqual(3);
+  });
+
+  test("should return 0 of Email", () => {
+    expect(countEmails("my email should be a@a.com")).toEqual(0);
+    expect(countEmails("my email should be @ bla .com")).toEqual(0);
+  });
+});
