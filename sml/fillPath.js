ffe7a2a637dde72c87c2e792aefe1875fadfc108
nghiemph95 <nghiemph95@gmail.com>
2022-06-09 16:17:21 +0700

    push

--- sml/fillPath.js
+++ sml/fillPath.js
@@ -1,1 +1,30 @@
-export function fillPath(path, params) {}
+export function fillPath(path, params) {
+  if (path.length <= 0 || typeof params !== "object") return "";
+
+  for (const key in params) {
+    path = path.replace(`:${key}`, params[key]);
+  }
+  return path;
+}
+
+// console.log(
+//   fillPath("/categories/:categoryId/products/:productId", {
+//     categoryId: 1,
+//     productId: 2,
+//   })
+// );
+
+// const arrObj = {
+//   categoryId: 1,
+//   productId: 2,
+// };
+
+// const arr = Object.values(arrObj);
+// console.log(arr);
+
+// for (const key in arrObj) {
+//   //   const array = [];
+//   console.log(key);
+// }
+
+// console.log("/categories/:categoryId/products/:productId".split("/"));
