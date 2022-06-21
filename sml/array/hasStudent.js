1af4008eb9775b1043b1e35873530b74b4c79539
nghiemph95 <nghiemph95@gmail.com>
2022-06-21 22:41:17 +0700

    u

--- sml/array/hasStudent.js
+++ sml/array/hasStudent.js
@@ -1,6 +1,16 @@
-export function hasStudent(studentList, studentId) {}
+export function hasStudent(studentList, studentId) {
+  for (let i = 0; i < studentList.length; i++) {
+    for (const key in studentList[i]) {
+      if (studentId === studentList[i][key]) {
+        return true;
+      }
+      return false;
+    }
+  }
+  const studentList = [
+    { id: 1, name: "Easy" },
+    { id: 2, name: "Frontend" },
+  ];
+}
 
-const studentList = [
-  { id: 1, name: "Easy" },
-  { id: 2, name: "Frontend" },
-];
+// console.log(hasStudent(studentList, 3));
