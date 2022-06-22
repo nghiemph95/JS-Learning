4759d89487a9414d3c6fba83dfee00aacbdc8b1c
nghiemph95 <nghiemph95@gmail.com>
2022-06-22 22:15:54 +0700

    push

--- sml/array/isSymmetricList.js
+++ sml/array/isSymmetricList.js
@@ -1,3 +1,10 @@
 export function isSymmetricList(numberList) {
   if (!Array.isArray(numberList) || numberList.length === 0) return false;
+
+  for (let i = 0; i < numberList.length / 2; i++) {
+    if (numberList[i] !== numberList[numberList.length - i - 1]) return false;
+  }
+  return true;
 }
+
+console.log(isSymmetricList([1, 2, 2, 1]));
