bb1d52336777ead74603d2412fbf92ea9c267b56
nghiemph95 <nghiemph95@gmail.com>
2022-06-22 15:50:39 +0700

    p

--- sml/array/hasFreeShip.js
+++ sml/array/hasFreeShip.js
@@ -1,5 +1,36 @@
-function 
+// const productList = [
+//   { id: 1, name: "Iphone 16", isFreeShip: false, price: 10200500 },
+//   { id: 2, name: "Iphone 16 Pro Max", isFreeShip: true, price: 1500000 },
+// ];
 
 export function hasFreeShip(productList, price) {
+  if (!Array.isArray(productList) || productList.length === 0) return false;
+  if (price < 0) return false;
 
+  function checkFreeShip(product) {
+    if (!product) return false;
+
+    return product.isFreeShip === true && price < product.price;
+  }
+
+  /**for i */
+  for (let i = 0; i < productList.length; i++) {
+    if (checkFreeShip(productList[i])) return true;
+  }
+  return false;
+
+  /** forEach */
+  let isFreeShip = false;
+  productList.forEach((element) => {
+    if (checkFreeShip(element)) isFreeShip = true;
+  });
+  return isFreeShip;
+
+  /** find*/
+  return Boolean(productList.find(checkFreeShip));
+
+  /** findIndex */
+  return productList.findIndex(checkFreeShip) > 0;
 }
+
+// console.log(hasFreeShip(productList, 1000000));
--- sml/array/hasFreeShip.test.js
+++ sml/array/hasFreeShip.test.js
@@ -6,6 +6,6 @@ describe("Name of the group testing", () => {
     { id: 2, name: "Iphone 16 Pro Max", isFreeShip: true, price: 1500000 },
   ];
   test("should return true", () => {
-    expect(hasFreeShip(productList, 10000000)).toBe(true);
+    expect(hasFreeShip(productList, 1000000)).toBe(true);
   });
 });
