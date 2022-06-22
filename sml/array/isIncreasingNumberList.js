4f9f6df0b7ffebf400345f1ca265668273f9070f
nghiemph95 <nghiemph95@gmail.com>
2022-06-22 16:21:38 +0700

    p

---
+++ sml/array/isIncreasingNumberList.js
@@ -0,0 +1,23 @@
+export function isIncreasingNumberList(numberList) {
+  if (!Array.isArray(numberList) || numberList.length === 0) return false;
+
+  /** for i  */
+  for (let i = 0; i < numberList.length; i++) {
+    if (numberList[i] < numberList[i + 1]) return true;
+  }
+  return false;
+
+  /** forEach */
+  let check = false;
+  numberList.forEach((_, i) => {
+    if (numberList[i] < numberList[i + 1]) check = true;
+  });
+  return check;
+
+  /** find */
+  //   return Boolean(
+  //     numberList.find((_, i) => {
+  //       numberList[i] < numberList[i + 1];
+  //     })
+  //   );
+}
---
+++ sml/array/isIncreasingNumberList.test.js
@@ -0,0 +1,11 @@
+import { isIncreasingNumberList } from "./isIncreasingNumberList";
+
+describe("Name of the group", () => {
+  test("should return true", () => {
+    expect(isIncreasingNumberList([1, 2])).toBe(true);
+  });
+  test("should return true", () => {
+    expect(isIncreasingNumberList([1, 1])).toBe(false);
+    expect(isIncreasingNumberList([2, 1])).toBe(false);
+  });
+});
