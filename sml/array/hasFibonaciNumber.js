ccb96ac7d25efea0a9d2dd719120a681fa6fe584
nghiemph95 <nghiemph95@gmail.com>
2022-06-26 00:39:50 +0700

    push

--- sml/array/hasFibonaciNumber.js
+++ sml/array/hasFibonaciNumber.js
@@ -1,1 +1,22 @@
-export function hasFibonaciNumber(numberList) {}
+function hasFibonaciNumber(numberList) {
+  if (!Array.isArray(numberList) || numberList.length === 0) return false;
+
+  let array = [0, 1];
+  let newArray = [];
+
+  for (let i = 2; i <= 11; i++) {
+    array[i] = array[i - 1] + array[i - 2];
+    newArray.push(array[i]);
+  }
+  console.log(newArray);
+
+  for (let i = 0; i < numberList.length; i++) {
+    const element = numberList[i];
+    for (let m = 0; m < newArray.length; m++) {
+      if (element === newArray[m]) return true;
+    }
+  }
+  return false;
+}
+
+// console.log(hasFibonaciNumber([0, 1, 2, 3]));
