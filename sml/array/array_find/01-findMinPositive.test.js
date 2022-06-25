4001c90eecf7c791e0458b24a74009ef6ec7d3c6
nghiemph95 <nghiemph95@gmail.com>
2022-06-26 01:34:42 +0700

    push

---
+++ sml/array/array_find/01-findMinPositive.test.js
@@ -0,0 +1,12 @@
+import { findMinPositive } from "./findMinPositive";
+
+describe("Name of the group", () => {
+  test("should return undefine", () => {
+    expect(findMinPositive([])).toBe(undefined);
+    expect(findMinPositive([-1, -5])).toBe(undefined);
+  });
+
+  test("should return undefine", () => {
+    expect(findMinPositive([-1, -5, 2, 6])).toBe(2);
+  });
+});
