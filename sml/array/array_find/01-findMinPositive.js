a020324aafc8f941ac9a0ad43b356347ab25c7c4
nghiemph95 <nghiemph95@gmail.com>
2022-06-26 16:40:04 +0700

    push

--- sml/array/array_find/01-findMinPositive.js
+++ sml/array/array_find/01-findMinPositive.js
@@ -1,30 +1,31 @@
 export function findMinPositive(numberList) {
   if (!Array.isArray(numberList) || numberList.length === 0) return;
 
-  // let newArray = [];
-  // for (let i = 0; i < numberList.length; i++) {
-  //   if (numberList[i] > 0) {
-  //     newArray.push(numberList[i]);
+  let newArray = [];
+  let res;
+  for (let i = 0; i < numberList.length; i++) {
+    if (numberList[i] > 0) {
+      newArray.push(numberList[i]).length === 0;
 
-  //     res = Math.min(...newArray);
+      res = Math.min(...newArray);
+    }
+  }
+  return res;
+
+  // let newArray = [];
+  // for (let j = 0; j < numberList.length; j++) {
+  //   if (numberList[j] > 0) {
+  //     newArray.push(numberList[j]).length === 0;
   //   }
   // }
-  // return res;
 
-  let newArray = [];
-  for (let j = 0; j < numberList.length; j++) {
-    if (numberList[j] > 0) {
-      newArray.push(numberList[j]).length === 0;
-    }
-  }
+  // let min = newArray[0];
 
-  let min = newArray[0];
-
-  for (let i = 1; i < newArray.length; i++) {
-    if (newArray[i] < min) {
-      min = newArray[i];
-    }
-  }
-  return min;
+  // for (let i = 1; i < newArray.length; i++) {
+  //   if (newArray[i] < min) {
+  //     min = newArray[i];
+  //   }
+  // }
+  // return min;
 }
-// console.log(findMinPositive([-1, -5]));
+console.log(findMinPositive([-1, -5]));
