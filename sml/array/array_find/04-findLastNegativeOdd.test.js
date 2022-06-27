f57a64ffe2062b30ab6aabec17d76545948eed3f
nghiemph95 <nghiemph95@gmail.com>
2022-06-27 17:35:40 +0700

    push

---
+++ sml/array/array_find/04-findLastNegativeOdd.test.js
@@ -0,0 +1,8 @@
+import { findLastNegativeOdd } from "./04-findLastNegativeOdd";
+
+describe("Name of the group", () => {
+  test("should ", () => {
+    expect(findLastNegativeOdd([-1, -3, -5])).toBe(-5);
+    expect(findLastNegativeOdd([1, 3, 5])).toBe();
+  });
+});
