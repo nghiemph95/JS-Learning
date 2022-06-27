65577084a0a1ba7381ef30d9bdd5b2b0d648c9f0
nghiemph95 <nghiemph95@gmail.com>
2022-06-28 00:13:45 +0700

    push

--- sml/array/array_find/05-findSecondLargestNumber.js
+++ sml/array/array_find/05-findSecondLargestNumber.js
@@ -1,1 +1,34 @@
-export function findSecondLargestNumber(numberList) {}
+export function findSecondLargestNumber(numberList) {
+  if (
+    !Array.isArray(numberList) ||
+    numberList.length === 0 ||
+    numberList.length === 1
+  )
+    return;
+
+  let newArray = [];
+  let max = numberList[0];
+
+  for (let i = 1; i < numberList.length; i++) {
+    if (numberList[i] > max) {
+      max = numberList[i];
+    }
+  }
+
+  const index = numberList.indexOf(max);
+
+  let maxi = numberList[0];
+  if (index > -1) {
+    numberList.splice(index, 1);
+
+    for (let m = 1; m < numberList.length; m++) {
+      if (numberList[m] > maxi) {
+        maxi = numberList[m];
+      }
+    }
+  }
+
+  return maxi;
+}
+
+console.log(findSecondLargestNumber([1, 2, 3]));
