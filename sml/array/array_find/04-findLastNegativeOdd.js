7b3bc2cf06932ff97382c40c3226d19855c49c8d
nghiemph95 <nghiemph95@gmail.com>
2022-06-27 22:50:15 +0700

    push

---
+++ sml/array/array_find/04-findLastNegativeOdd.js
@@ -0,0 +1,30 @@
+export function findLastNegativeOddV1(numberList) {
+  if (!Array.isArray(numberList) || numberList.length === 0) return;
+
+  let newArray = [];
+  for (let i = 0; i < numberList.length; i++) {
+    if (numberList[i] % 2 !== 0 && numberList[i] < 0) {
+      newArray.push(numberList[i]);
+    }
+  }
+  return newArray[newArray.length - 1];
+}
+
+export function findLastNegativeOddV2(numberList) {
+  if (!Array.isArray(numberList) || numberList.length === 0) return;
+
+  let newArray = [];
+  numberList.forEach((element) => {
+    if (element % 2 !== 0 && element < 0) {
+      newArray.push(element);
+    }
+  });
+  return newArray[newArray.length - 1];
+}
+
+export function findLastNegativeOdd(numberList) {
+  if (!Array.isArray(numberList) || numberList.length === 0) return;
+
+  return numberList.slice(-1).find((e) => e % 2 !== 0 && e < 0);
+}
+// console.log(findLastNegativeOdd([1, 3, 5]));
