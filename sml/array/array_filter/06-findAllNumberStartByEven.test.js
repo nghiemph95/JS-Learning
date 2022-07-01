9249e98cd15b4969c418ed3add934846095b1621
nghiemph95 <nghiemph95@gmail.com>
2022-07-02 00:53:06 +0700

    push

--- sml/array/array_filter/06-findAllNumberStartByEven.test.js
+++ sml/array/array_filter/06-findAllNumberStartByEven.test.js
@@ -1,5 +1,8 @@
 import { findAllNumber } from "./06-findAllNumberStartByEven";
 
 describe("Name of the group", () => {
-  test("should ", () => {});
+  test("should ", () => {
+    expect(findAllNumber([1, 5, 6])).toStrictEqual([1, 5]);
+    expect(findAllNumber([234, 421, 123])).toStrictEqual([123]);
+  });
 });
