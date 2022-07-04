f3f924a2522c0266cfa80fbe8344e9cfa10d6b0e
nghiemph95 <nghiemph95@gmail.com>
2022-07-04 23:51:23 +0700

    push

--- sml/array/array_count/04-countUniqueNumbers.js
+++ sml/array/array_count/04-countUniqueNumbers.js
@@ -24,9 +24,45 @@ export function countUniqueNumbersV2(numberList) {
 
 console.log(countUniqueNumbersV2([1, 2, 3]));
 
-export function countUniqueNumbers(numberList) {
+export function countUniqueNumbersV3(numberList) {
   if (!Array.isArray(numberList) || numberList.length === 0) return 0;
 
   return new Set(numberList).size;
 }
 console.log(countUniqueNumbers([1, 1, 1]));
+
+export function countUniqueNumbersV4(numberList) {
+  if (!Array.isArray(numberList) || numberList.length === 0) return 0;
+
+  const res = [];
+
+  for (let i = 0; i < numberList.length; i++) {
+    if (!res.includes(numberList[i])) res.push(numberList[i]);
+  }
+
+  return res.length;
+}
+
+export function countUniqueNumbersV5(numberList) {
+  if (!Array.isArray(numberList) || numberList.length === 0) return 0;
+
+  const count = {};
+  for (let i = 0; i < numberList.length; i++) {
+    count[numberList[i]] = true;
+    console.log(count[numberList[i]]);
+  }
+  return Object.keys(count).length;
+}
+
+export function countUniqueNumbers(numberList) {
+  if (!Array.isArray(numberList) || numberList.length === 0) return 0;
+
+  const count = numberList.reduce((count, number) => {
+    count[number] = true;
+    return count;
+  }, {});
+
+  return Object.keys(count).length;
+}
+
+console.log(countUniqueNumbers([1, 2, 3]));
