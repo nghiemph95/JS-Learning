cfbfcc841b0b0c435d0c988acdae8c182dd4d49e
nghiemph95 <nghiemph95@gmail.com>
2022-07-05 00:55:46 +0700

    push

--- sml/array/array_count/05-countNumbersNotInB.js
+++ sml/array/array_count/05-countNumbersNotInB.js
@@ -1,2 +1,38 @@
-/** Viết hàm countNumbersNotInB(a, b) để đếm có bao nhiếu số xuất hiện trong mảng a, mà không có trong
-mảng b */
+/** Viết hàm countNumbersNotInB(a, b) để đếm
+ * có bao nhiếu số xuất hiện trong mảng a,
+ *  mà không có trong mảng b */
+export function countNumbersNotInBV1(a, b) {
+  if (!Array.isArray(a, b) && a.length === 0 && b.length === 0) return 0;
+
+  let count = 0;
+  for (let i = 0; i < a.length; i++) {
+    for (let m = 0; m < b.length; m++) {
+      if (a[i] !== b[m]) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
+
+// console.log(countNumbersNotInB([1, 2, 3], [4, 5]));
+
+export function countNumbersNotInBV2(a, b) {
+  if (!Array.isArray(a, b) && a.length === 0 && b.length === 0) return 0;
+
+  return a.filter((element) => !b.includes(element)).length;
+}
+
+export function countNumbersNotInB(a, b) {
+  if (!Array.isArray(a, b) && a.length === 0 && b.length === 0) return 0;
+  let count = 0;
+  a.filter((element) => {
+    if (b.indexOf(element) < 0) {
+      count++;
+    }
+  });
+
+  return count;
+}
+// console.log(countNumbersNotInB([1, 2, 3], [3, 4, 5]));
---
+++ sml/array/array_count/05-countNumbersNotInB.test.js
@@ -0,0 +1,13 @@
+import { countNumbersNotInB } from "./05-countNumbersNotInB";
+
+describe("Name of the group", () => {
+  test("should return value", () => {
+    expect(countNumbersNotInB([1, 2, 3], [4, 5])).toBe(3);
+  });
+  test("should return value", () => {
+    expect(countNumbersNotInB([1, 2, 3], [1, 2, 3])).toBe(0);
+  });
+  test("should return value", () => {
+    expect(countNumbersNotInB([1, 2, 3], [3, 4, 5])).toBe(2);
+  });
+});
