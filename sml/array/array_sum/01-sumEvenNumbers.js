45b29bc77c7f75b279da1371b8c9e98164215143
nghiemph95 <nghiemph95@gmail.com>
2022-07-06 21:52:42 +0700

    d

---
+++ sml/array/array_sum/01-sumEvenNumbers.js
@@ -0,0 +1,13 @@
+export function sumEvenNumbers(numberList) {
+  if (!Array.isArray(numberList) || numberList.length === 0) return;
+  let array = [];
+  for (let i = 0; i < numberList.length; i++) {
+    if (numberList[i] > numberList[i - 1] && numberList[i] % 2 === 0) {
+      array.push(numberList[i]);
+      console.log(array);
+    }
+  }
+
+  return array.reduce((a, b) => a + b);
+}
+console.log(sumEvenNumbers([-10, -4, 2, 8, 5]));
