bfc183687b3c702c5cb7d95adb6437fc7565f5f8
nghiemph95 <nghiemph95@gmail.com>
2022-07-11 22:54:51 +0700

    s

--- sml/array/array_subarr/03-isSubArray.js
+++ sml/array/array_subarr/03-isSubArray.js
@@ -3,10 +3,18 @@ Nếu a là mảng rỗng thì luôn trả về true.
 Nếu a có độ dài lớn hơn b thì luôn trả về false.
 Trả về true nếu toàn bộ mảng a nằm trong mảng b theo đúng thứ tự của từng phần tử trong mảng a. */
 
-export function isSubArray(a, b) {
+export function isSubArrayV1(a, b) {
   if (!Array.isArray(a, b) || b.length === 0) return false;
 
   if (a.length === 0 && b.length > 0) return true;
 
   return a.every((element) => b.includes(element));
+}
+
+export function isSubArray(a, b) {
+  if (!Array.isArray(a, b) || b.length === 0) return false;
+
+  if (a.length === 0 && b.length > 0) return true;
+
+  return a.every((element) => b.indexOf(element) >= 0);
 }
