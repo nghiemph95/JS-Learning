7ded3e4d616b57b812d0624740519f3c8c9fc764
nghiemph95 <nghiemph95@gmail.com>
2022-07-14 13:19:38 +0700

    s

--- sml/coding/06-mergeArray.js
+++ sml/coding/06-mergeArray.js
@@ -1,10 +1,17 @@
+export function mergeArrayV1(a, b) {
+  if (!Array.isArray(a, b) || (a.length === 0 && b.length === 0)) return [];
+
+  if (b === true || a === true) return [];
+
+  return [...new Set([...a, ...b])];
+}
+
 export function mergeArray(a, b) {
   if (!Array.isArray(a, b) || (a.length === 0 && b.length === 0)) return [];
 
   if (b === true || a === true) return [];
-  //   let res = [];
-  //   res = a.concat(b).unique();
-  return [...new Set([...a, ...b])];
+
+  return a.concat(b).filter((element, i) => a.concat(b).indexOf(element) === i);
 }
 
 console.log(mergeArray([1, 2, 3], [2, 3, 4]));
