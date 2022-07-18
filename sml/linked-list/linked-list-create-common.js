5b5914d1fcd2b55d731b3a22bbea35709fcc8ac2
nghiemph95 <nghiemph95@gmail.com>
2022-07-18 23:25:51 +0700

    is

---
+++ sml/linked-list/linked-list-create-common.js
@@ -0,0 +1,114 @@
+/** getHead()
+ * getTail()
+ * getSize()
+ * findIndexByData()
+ * findWithCallback()
+ */
+
+function createLinkedList() {
+  let head = null;
+
+  function getHead() {
+    return head;
+  }
+
+  function getTail() {
+    if (head == null) return null;
+
+    let tail = head;
+    while (tail.next != null) {
+      //   console.log(tail.data);
+      tail = tail.next;
+    }
+    return tail;
+  }
+
+  function getSize() {
+    if (head == null) return 0;
+
+    let current = head;
+    let count = 0;
+    while (current != null) {
+      count++;
+      current = current.next;
+    }
+    return count;
+  }
+
+  function findIndexByData(data) {
+    if (head == null) return -1;
+
+    let current = head;
+    let index = 0;
+    while (current != null) {
+      if (current.data === data) return index;
+      index++;
+      current = current.next;
+    }
+    return -1;
+  }
+
+  function findWithCallback(callback) {
+    if (head == null) return;
+
+    let current = head;
+    let index = 0;
+    while (current != null) {
+      if (callback(current.data, index)) return current.data;
+      index++;
+      current = current.next;
+    }
+    return;
+  }
+
+  function insertHead(newData) {
+    const newNode = { data: newData, next: null };
+
+    if (head == null) {
+      head = newNode;
+      return;
+    }
+
+    newNode.next = head;
+    head = newNode;
+  }
+  function printList() {
+    if (head == null) return;
+
+    let current = head;
+    while (current != null) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+
+  return {
+    getHead,
+    getTail,
+    getSize,
+    findIndexByData,
+    findWithCallback,
+    insertHead,
+    printList,
+  };
+}
+
+const numberLinkedList = createLinkedList();
+
+numberLinkedList.insertHead(5);
+numberLinkedList.insertHead(4);
+numberLinkedList.insertHead(3);
+numberLinkedList.insertHead(2);
+numberLinkedList.insertHead(1);
+
+console.log("Head", numberLinkedList.getHead());
+console.log("Tail", numberLinkedList.getTail());
+console.log("Size", numberLinkedList.getSize());
+
+console.log(numberLinkedList.findIndexByData(3)); // -> 2
+console.log(numberLinkedList.findIndexByData(6)); // -> -1
+
+console.log(numberLinkedList.findWithCallback((x, idx) => x % 2 === 0)); // -> 2
+console.log(numberLinkedList.findWithCallback((x, idx) => x > 5)); // -> undefined
+
+numberLinkedList.printList();
