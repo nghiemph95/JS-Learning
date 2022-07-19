bb8619e33ddd35ce7c59932b422530bfef94f1ba
nghiemph95 <nghiemph95@gmail.com>
2022-07-19 16:15:19 +0700

    s

---
+++ sml/linked-list/linked-list-every.js
@@ -0,0 +1,201 @@
+/** some(isValidFn)
+ */
+
+function createLinkedList() {
+  let head = null;
+
+  //Insert Tail
+  function insertTail(data) {
+    const newNode = { data: data, next: null };
+
+    if (!head) {
+      head = newNode;
+    } else {
+      let lastNode = head;
+      while (lastNode.next != null) {
+        lastNode = lastNode.next;
+      }
+      lastNode.next = newNode;
+      /** Other solution */
+      //   const tail = getTail();
+      //   tail.next = newNode;
+    }
+    return head;
+  }
+
+  // Insert Before Position
+  function insertBeforePosition(data, position) {
+    if (position <= 0 || head == null) {
+      return insertHead(data);
+    }
+
+    const newNode = { data: data, next: null };
+
+    let prev = head;
+    let curr = head;
+    let i = 0;
+
+    while (curr != null && i < position) {
+      prev = curr;
+      curr = curr.next;
+      i++;
+    }
+    newNode.next = curr;
+    prev.next = newNode;
+    return head;
+  }
+
+  function getHead() {
+    return head;
+  }
+
+  function getTail() {
+    if (head == null) return null;
+
+    let tail = head;
+    while (tail.next != null) {
+      //   console.log(tail.data);
+      tail = tail.next;
+    }
+    return tail;
+  }
+
+  function getSize() {
+    if (head == null) return 0;
+
+    let current = head;
+    let count = 0;
+    while (current != null) {
+      count++;
+      current = current.next;
+    }
+    return count;
+  }
+
+  function findIndexByData(data) {
+    if (head == null) return -1;
+
+    let current = head;
+    let index = 0;
+    while (current != null) {
+      if (current.data === data) return index;
+      index++;
+      current = current.next;
+    }
+    return -1;
+  }
+
+  function findWithCallback(callback) {
+    if (head == null) return;
+
+    let current = head;
+    let index = 0;
+    while (current != null) {
+      if (callback(current.data, index)) return current.data;
+      index++;
+      current = current.next;
+    }
+    return;
+  }
+
+  function insertHead(newData) {
+    const newNode = { data: newData, next: null };
+
+    if (head == null) {
+      head = newNode;
+      return;
+    }
+
+    newNode.next = head;
+    head = newNode;
+  }
+
+  function some(isValidFn) {
+    if (head == null) return false;
+
+    let current = head;
+
+    while (current != null) {
+      if (isValidFn(current.data)) return true;
+
+      current = current.next;
+    }
+    return false;
+  }
+
+  function every(isValidFn) {
+    if (head == null) return false;
+
+    let current = head;
+
+    while (current != null) {
+      if (isValidFn(current.data)) return true;
+      current = current.next;
+    }
+    return false;
+  }
+
+  function printList() {
+    if (head == null) return;
+
+    let current = head;
+    while (current != null) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+
+  return {
+    getHead,
+    getTail,
+    getSize,
+    insertTail,
+    insertBeforePosition,
+    findIndexByData,
+    findWithCallback,
+    insertHead,
+    some,
+    every,
+    printList,
+  };
+}
+
+const numberLinkedList = createLinkedList();
+
+numberLinkedList.insertHead(5);
+numberLinkedList.insertHead(4);
+numberLinkedList.insertHead(3);
+numberLinkedList.insertHead(2);
+numberLinkedList.insertHead(1);
+
+numberLinkedList.insertTail(6);
+
+numberLinkedList.insertBeforePosition(3, 2);
+
+console.log("Head", numberLinkedList.getHead());
+console.log("Tail", numberLinkedList.getTail());
+console.log("Size", numberLinkedList.getSize());
+
+console.log("findIndexByData", numberLinkedList.findIndexByData(3)); // -> 2
+console.log("findIndexByData", numberLinkedList.findIndexByData(6)); // -> -1
+
+console.log(
+  "findWithCallback",
+  numberLinkedList.findWithCallback((x, idx) => x % 2 === 0)
+); // -> 2
+console.log(
+  "findWithCallback",
+  numberLinkedList.findWithCallback((x, idx) => x > 5)
+); // -> undefined
+
+console.log(
+  "some(isValidFn(x => x < 6))",
+  numberLinkedList.some((x) => x < 6)
+); // -> true
+
+console.log(
+  "every(isValidFn(x => x > 1))",
+  numberLinkedList.every((x) => x > 1)
+); // -> true
+
+numberLinkedList.printList();
