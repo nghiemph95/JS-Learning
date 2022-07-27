74a9baddd86d7bda02fb7dcf6831db3b71f95fb4
nghiemph95 <nghiemph95@gmail.com>
2022-07-27 19:43:56 +0700

    s

---
+++ sml/dataStructure/JS built in object_Map/map.js
@@ -0,0 +1,30 @@
+// Using ES6
+const cityList = [
+  { id: 1, name: "TP. Hồ Chí Minh" },
+  { id: 2, name: "TP. Phan Thiết" },
+];
+
+const studentList = [
+  { id: 123, name: "Alice", cityId: 1 },
+  { id: 321, name: "Bob", cityId: 2 },
+];
+
+// How to show city name for each student?
+const cityMap = cityList.reduce((map, city) => {
+  map[city.id] = city;
+  console.log(map);
+  return map;
+}, {});
+
+// Using Map
+const cityMaps = new Map();
+cityList.forEach((element) => {
+  cityMaps.set(element.id, element);
+});
+
+const cityMapss = cityList.reduce((map, city) => {
+  map.set(city.id, city);
+  return map;
+}, new Map());
+
+console.log(cityMapss.get(1).name);
