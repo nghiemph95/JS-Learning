1ee8e74b094e5fd0bebe131611a910f4392e0724
nghiemph95 <nghiemph95@gmail.com>
2022-08-03 23:34:13 +0700

    s

--- sml/algorithm/quickSort.js
+++ sml/algorithm/quickSort.js
@@ -1,5 +1,27 @@
 function partition(numberList, left, right) {
-  const mid = left + Math.trunc((left + right) / 2);
+  const mid = left + Math.trunc((right - left) / 2);
+  const pivot = numberList[mid];
+
+  let i = left;
+  let j = right;
+
+  while (i <= j) {
+    // find the first item that greater/equal than pivot
+    while (numberList[i] < pivot) i++;
+    // find the first item that smaller/equal than pivot
+    while (numberList[j] > pivot) j--;
+
+    if (i < j) {
+      const temp = numberList[i];
+      numberList[i] = numberList[j];
+      numberList[j] = temp;
+    }
+    if (i <= j) {
+      i++;
+      j--;
+    }
+  }
+  return i;
 }
 
 function quickSort(numberList, left, right) {
