5ca52124d19dc196eef50357e6328a86bd3bdfb3
nghiemph95 <nghiemph95@gmail.com>
2022-08-04 15:28:21 +0700

    s

--- sml/algorithm/sort.js
+++ sml/algorithm/sort.js
@@ -4,8 +4,33 @@ const studentList = [
   { id: 2, name: "John", marks: { math: 10, english: 10 } },
 ];
 
-function sortById() {}
+function sortById() {
+  if (!Array.isArray(studentList) || studentList.length === 0) return [];
 
-function sortByName() {}
+  return studentList.sort((s1, s2) => s1.id - s2.id);
+}
+console.log("sortById", sortById(studentList));
 
-function sortByAvg() {}
+function sortByName() {
+  if (!Array.isArray(studentList) || studentList.length === 0) return [];
+
+  return studentList.sort((s1, s2) => {
+    if (s1.name > s2.name) return 1;
+    if (s1.name < s2.name) return -1;
+    return 0;
+  });
+}
+console.log("sortByName", sortByName(studentList));
+
+function sortByAvg() {
+  if (!Array.isArray(studentList) || studentList.length === 0) return [];
+
+  return studentList.sort((s1, s2) => {
+    const s1Avg = (s1.marks.math + s1.marks.english) / 2;
+    const s2Avg = (s2.marks.math + s2.marks.english) / 2;
+
+    return s1Avg - s2Avg;
+  });
+}
+
+console.log("sortByAvg", sortByAvg(studentList));
