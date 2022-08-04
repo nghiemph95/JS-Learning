1e4a51241b8898a27b59ce30df013f910a033e66
nghiemph95 <nghiemph95@gmail.com>
2022-08-04 13:27:17 +0700

    s

---
+++ sml/algorithm/sort.js
@@ -0,0 +1,11 @@
+const studentList = [
+  { id: 1, name: "Alice", marks: { math: 9, english: 10 } },
+  { id: 3, name: "Bob", marks: { math: 5, english: 10 } },
+  { id: 2, name: "John", marks: { math: 10, english: 10 } },
+];
+
+function sortById() {}
+
+function sortByName() {}
+
+function sortByAvg() {}
