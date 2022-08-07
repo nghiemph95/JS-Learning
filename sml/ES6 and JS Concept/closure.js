868859fa3e9ff78aa9bf6114858e74ab1485bc44
nghiemph95 <nghiemph95@gmail.com>
2022-08-07 20:08:07 +0700

    s

--- sml/ES6 and JS Concept/closure.js
+++ sml/ES6 and JS Concept/closure.js
@@ -12,3 +12,33 @@ function outerFunc() {
 const myInnerFunc = outerFunc();
 
 myInnerFunc();
+
+//Example
+function createCounter(initValue) {
+  let value = initValue || 0;
+
+  function increase() {
+    value++;
+  }
+
+  function decrease() {
+    value--;
+  }
+
+  function getValue() {
+    return value;
+  }
+
+  return {
+    increase,
+    decrease,
+    getValue,
+  };
+}
+
+const counter1 = createCounter(0);
+counter1.increase();
+counter1.decrease();
+counter1.getValue();
+
+const counter2 = createCounter(10);
