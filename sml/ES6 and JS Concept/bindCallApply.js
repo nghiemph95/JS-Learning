888e163840addb912ec4453477204e8c74c9689d
nghiemph95 <nghiemph95@gmail.com>
2022-08-08 13:41:58 +0700

    s

--- sml/ES6 and JS Concept/bindCallApply.js
+++ sml/ES6 and JS Concept/bindCallApply.js
@@ -14,3 +14,6 @@ const student = {
 const studentSayHello = sayHello.bind(student); // this chính là student nên khi vào hàm sayHello, chỗ nào là this nó sẽ được thay thế bằng hàm student
 // this.name sẽ tương đương student.name
 studentSayHello(5, 10); // Nếu không chủ động gọi hàm này thì hàm bind sẽ ko chạy mà để đó.
+
+sayHello.call(student, 2, 3);
+sayHello.apply(student, [6, 10]);
