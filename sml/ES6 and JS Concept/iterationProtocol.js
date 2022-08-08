c1a1d4f1b544e5ed050726c1f7c3668f1c3a5238
nghiemph95 <nghiemph95@gmail.com>
2022-08-08 14:41:26 +0700

    s

--- sml/ES6 and JS Concept/iterationProtocol.js
+++ sml/ES6 and JS Concept/iterationProtocol.js
@@ -1,3 +1,4 @@
+/** Create Iterator */
 function createIterator(n) {
   let i = 0;
 
@@ -22,4 +23,36 @@ while (!result.done) {
   console.log(result.value);
 
   result = interator.next();
+}
+
+for (const number of interator) {
+  console.log(number); // interator is not iterable
+}
+
+/** Create Iterable */
+function createIterable(n) {
+  let i = 0;
+
+  return {
+    //(){} zero-argument function
+    [Symbol.iterator]() {
+      return {
+        next() {
+          const result = {
+            value: [i, i * 2],
+            done: i >= n,
+          };
+          //inscrease counter
+          i += 5;
+
+          return result;
+        },
+      };
+    },
+  };
+}
+
+const iterable = createIterable(50);
+for (const [number, numberX2] of iterable) {
+  console.log(number, numberX2);
 }
