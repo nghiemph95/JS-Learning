48fba920e5ff78bd4a4fda214ed46c0c293447cd
nghiemph95 <nghiemph95@gmail.com>
2022-08-10 16:46:23 +0700

    Updated: 10-08-2022 16:46:23

--- sml/ECMAScript/es2019.js
+++ sml/ECMAScript/es2019.js
@@ -32,3 +32,70 @@ Object.fromEntries(entries); // Sau khi gọi lại entries nó sẽ tạo lại
 // id: 1,
 // name: 'Easy Frontend',
 // }
+
+/** String.prototype.{trimStart, trimEnd} */
+" Easy Frontend ".trim(); // 'Easy Frontend' - bỏ khoảng trắng cho cả đầu và cuối
+// ES2019 - bỏ khoảng trắng đầu hoặc cuối
+"Easy Frontend".trimStart(); // 'Easy Frontend'
+" Easy Frontend ".trimStart(); // 'Easy Frontend '
+"Easy Frontend".trimEnd(); // 'Easy Frontend'
+" Easy Frontend ".trimEnd(); // ' Easy Frontend'
+
+/** Symbol.prototype.description - Thêm phần mô tả cho Symbol */
+const sym = Symbol("description goes here");
+sym.description; // 'description goes here'
+sym.toString(); // 'Symbol(desc)'
+
+/** Optional catch binding */
+// Trước ES2019
+function parseStudentFromJSON(str) {
+  try {
+    return JSON.parse(str);
+  } catch (error) {
+    // in case of failed to parse, simply return undefined
+    return undefined;
+    // error is unused 😭
+  }
+}
+
+// Sau ES2019
+function parseStudentFromJSON(str) {
+  try {
+    return JSON.parse(str);
+  } catch {
+    // in case of failed to parse, simply return undefined
+    return undefined;
+  }
+}
+
+/** Array.prototype.sort() is guaranteed to be stable - trong trường hợp những phần tử bằng nhau thì nó sẽ giữ nguyên thứ tự trong
+ * mảng ban đầu
+ */
+[
+  { id: 1, name: "Alice" }, // Alice giống nhau
+  { id: 3, name: "John" },
+  { id: 2, name: "Bob" },
+  { id: 4, name: "Alice" }, // Alice giống nhau nhưng vẫn giữ nguyên thứ tự ban đầu
+].sort((s1, s2) => {
+  if (s1.name > s2.name) return 1;
+  if (s1.name < s2.name) return -1;
+  return 0;
+});
+// output
+// [
+// { id: 1, name: 'Alice' },
+// { id: 4, name: 'Alice' },
+// { id: 2, name: 'Bob' },
+// { id: 3, name: 'John' },
+// ]
+
+/** Revised Function.prototype.toString() */
+function /* this is bar */ bar() {}
+// BEFORE
+bar.toString(); //'function bar() {}
+// ES2019
+bar.toString(); // 'function /* this is bar */ bar () {}'
+
+// for built-in function
+Math.trunc.toString();
+// function trunc() { [native code] }
