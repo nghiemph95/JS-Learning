c3413ca26ad4b51546696a23ec8327f1d2a5187b
nghiemph95 <nghiemph95@gmail.com>
2022-08-10 11:21:22 +0700

    Updated: 2022-08-10 11:21:22

--- push.sh
+++ push.sh
@@ -2,8 +2,8 @@ git add .
 
 # echo 'Enter the commit message:'
 
-current="`date +'%Y-%m-%d %H:%M:%S'`"
-msg="Updated: $current"
+currentDate="`date +'%Y-%m-%d %H:%M:%S'`"
+msg="Updated: $currentDate"
 git commit -m "$msg" # Updated: 2019-08-28 10:22:06
 
 # echo 'Enter the git pushing source code:'
