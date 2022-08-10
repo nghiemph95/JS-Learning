eb99a76a31118d32474d5015996cec690f15a083
nghiemph95 <nghiemph95@gmail.com>
2022-08-10 11:12:24 +0700

    GIT_AUTHOR_DATE=

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@ git add .
 # echo 'Enter the commit message:'
 
 
-GIT_AUTHOR_DATE=$(date -d'...') GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE" git commit -m'$(date -d'...')'
+GIT_AUTHOR_DATE=$(date -d'...') GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE" git commit -m GIT_AUTHOR_DATE=$(date -d'...')
 
 # echo 'Enter the git pushing source code:'
 # read push
