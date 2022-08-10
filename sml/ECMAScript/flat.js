b2e9550431e61169ba62d819279b588b554d46b1
nghiemph95 <nghiemph95@gmail.com>
2022-08-10 17:15:37 +0700

    Updated: 10-08-2022 17:15:37

---
+++ sml/ECMAScript/flat.js
@@ -0,0 +1,13 @@
+/** làm phẳng array
+ *  [1,2,3,[4]] => [1,2,3,4]
+ *  [1,2,[[3], 4]] => [1,2,[3],4] => [1,2,3,4]
+ */
+
+const arr = [1, 2, [3, [4]]];
+//Using flat()
+console.log(arr.flat());
+//Using reduce - concat
+console.log(arr.reduce((res, value) => res.concat(value), []));
+//decomposition syntax
+const resultFlat = (arr) => [].concat(...arr);
+console.log(resultFlat);
