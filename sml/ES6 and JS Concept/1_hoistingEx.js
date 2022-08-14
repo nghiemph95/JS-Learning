ec2dfb4d021e86d957b36bcc86d6b3ee7546fe4e
nghiemph95 <nghiemph95@gmail.com>
2022-08-14 11:25:03 +0700

    Updated: 14-08-2022 11:25:03

--- sml/ES6 and JS Concept/1_hoistingEx.js
+++ sml/ES6 and JS Concept/1_hoistingEx.js
@@ -2,8 +2,8 @@ var tip = 100;
 
 (function () {
   console.log("I have $" + husband());
-  /** complier auto hoisitng to desclaration var tip; before run code
-   * and var tip = undefined
+  /** complier auto hoisitng to desclaration(Khai báo) var tip; before run code
+   * and var tip = undefined (biến tip khi khai báo mà không gán giá trị sẽ trả về undefined)
    * then undefined * 2 = NaN
    */
 
