909bddf7b925a0859773a6e1a1d12a7f8c07716e
nghiemph95 <nghiemph95@gmail.com>
2022-08-15 13:17:53 +0700

    Updated: 15-08-2022 13:17:53

--- sml/async programing/1_setTimeout.js
+++ sml/async programing/1_setTimeout.js
@@ -33,3 +33,33 @@ console.log("log 3");
 // log 1
 // log 3
 // log 2 🤣 Sau khi chạy hết các script bên ngoài rồi mới chạy setTimeout()
+
+console.log("Xin chào các bạn");
+setTimeout(() => {
+  console.log("ông thần này!");
+}, 1000);
+console.log("Mình là Hậu :P");
+// Real case: Redirect to another page after 3 seconds
+setTimeout(() => {
+  window.location.href = "https://google.com";
+}, 3000);
+
+/** Clear timeOut */
+const timeoutId1 = setTimeout(() => {
+  console.log("Tada!!!");
+}, 5000);
+
+clearTimeout(timeoutId1); // cancel the timeout
+// should be called before the timeout expired
+
+/** Late timeOut */
+setTimeout(() => {
+  console.log("Tada!!!");
+});
+
+let count = 1;
+for (let i = 0; i < 1e9; i++) {
+  count++;
+}
+
+console.log("done");
