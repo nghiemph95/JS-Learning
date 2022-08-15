f9dfdfcffd416a01f224f9e3b0978c8f7dec14a5
nghiemph95 <nghiemph95@gmail.com>
2022-08-15 16:12:02 +0700

    Updated: 15-08-2022 16:12:02

---
+++ sml/async programing/2_debounce.js
@@ -0,0 +1,14 @@
+/** giảm thiếu só lần trigger số lần even
+ * Ứng dụng: Dùng khi người dùng nhập text để tìm kiếm
+ * Ứng dung: Dùng để lấy kết quả resize cuối cùng của browser
+ */
+
+/** how it work */
+function debounce(callback, wait) {}
+
+function log() {}
+
+const debounceLog = debounce(log, wait);
+debounceLog();
+debounceLog();
+debounceLog();
