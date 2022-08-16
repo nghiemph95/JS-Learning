30cebaaa4a3ee2c0df19debf6c19fb706ceca48b
nghiemph95 <nghiemph95@gmail.com>
2022-08-16 09:02:07 +0700

    Updated: 16-08-2022 09:02:07

---
+++ sml/async programing/3_throttle.js
@@ -0,0 +1,34 @@
+/** throttle trigger cái hàm của mình tối đa 1 lần trong khoảng thời gian quy ước */
+function log() {
+  console.log("tada");
+}
+
+function throttle(callback, wait) {
+  let isThrottling = false;
+
+  return function () {
+    if (isThrottling) return; // 2, 3, 4 come here
+
+    isThrottling = true; // 1 come here
+    setTimeout(() => {
+      callback();
+
+      isThrottling = false;
+    }, wait);
+  };
+}
+
+const throttleLog = throttle(log, 500);
+
+throttleLog(); //set timeout 500 -> call -> turn off flag throttle
+throttleLog(); // check throttling -> ignore
+throttleLog(); // check throttling -> ignore
+throttleLog(); // check throttling -> ignore
+throttleLog(); // check throttling -> ignore
+
+setTimeout(throttleLog, 600); // start throttling
+setTimeout(throttleLog, 600);
+setTimeout(throttleLog, 600);
+setTimeout(throttleLog, 600);
+setTimeout(throttleLog, 700);
+setTimeout(throttleLog, 700);
