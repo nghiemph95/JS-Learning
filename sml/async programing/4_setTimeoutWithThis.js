b333ec78898328ee4c66623a4d4dc79eae1d7470
nghiemph95 <nghiemph95@gmail.com>
2022-08-16 10:28:12 +0700

    Updated: 16-08-2022 10:28:12

--- sml/async programing/4_setTimeoutWithThis.js
+++ sml/async programing/4_setTimeoutWithThis.js
@@ -9,4 +9,16 @@ const student = {
 
 student.sayHello(); // this trong method sẽ trở đến object mà gọi cái method đó là sayHello()
 
+//Thằng gọi không phải thằng Student
+// setTimeout sẽ chạy qua webAPI, sau khi đó mới chạy qua callback queue, nên hàm được gọi là từ 1 context khác
 setTimeout(student.sayHello);
+
+// setTimeout sẽ không dùng bind this được
+setTimeout.bind(student, student.sayHello); // Dính lỗi Illegal invocation
+
+//Cách sử lý sử dụng wrapper function
+setTimeout(() => {
+  console.log("Cách sử lý sử dụng wrapper function");
+  console.log("this", this); //This sẽ trả ra global object
+  student.sayHello();
+});
