c92d66280a149c737c6a403b49e1c7088b90a3ff
nghiemph95 <nghiemph95@gmail.com>
2022-08-16 21:59:05 +0700

    Updated: 16-08-2022 21:59:05

---
+++ sml/async programing/6_eventLoop.js
@@ -0,0 +1,15 @@
+/** JavaScript Engine (V8): Take code JS
+ * -> Parse it
+ * -> Abstract syntax tree
+ * -> generate bytecode
+ * -> get feedback
+ * -> optimize & compile it
+ * -> CPU */
+
+/** ECMAScript Engines:
+ * - Chrome: V8
+ * - NodeJS: V8
+ * - Mircrosoft Edge: V8
+ * - Firefox: Spider Monkey
+ * - Safari: JavaScript Core
+ */
