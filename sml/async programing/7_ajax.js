ce9d8d8316bf292c9cc2be56e65c78cd32967b6e
nghiemph95 <nghiemph95@gmail.com>
2022-08-17 21:48:22 +0700

    Updated: 17-08-2022 21:48:22

--- sml/async programing/7_ajax.js
+++ sml/async programing/7_ajax.js
@@ -1,4 +1,7 @@
 /** AJAX được tạo thành từ nhiều công nghệ khác nhau, bao gồm HTML/CSS, DOM
  * và XMLHttpRequest: gửi dữ liệu lên server và đợi server trả về
  * XML,HTML, XSLT
+ * Túm lại: Chuẩn gồm tập hợp nhìu công nghệ vs nhau từ request đến response
  */
+
+/** HTTP APIs : Chuẩn giao tiếp giữa 2 hệ thống với nhau */
