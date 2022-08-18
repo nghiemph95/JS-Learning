d4120fac2e236f8ba56bcfa26715030e43b07f3f
nghiemph95 <nghiemph95@gmail.com>
2022-08-18 11:26:45 +0700

    Updated: 18-08-2022 11:26:45

--- sml/async programing/8_promise.js
+++ sml/async programing/8_promise.js
@@ -2,3 +2,24 @@
  * nếu success: sử dụng .then() để lấy thông tin
  * nếu error: sử dụng .catch() để trả về error
  */
+
+/** tạo promise */
+const promise = new Promise((resolve, reject) => {});
+console.log(promise);
+
+/** retrieve promise value */
+const promiseV1 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve([1, 2, 3, 4]), 3000);
+});
+promiseV1
+  .then((result) => console.log(result)) // kết quả sẽ láy theo giá trị resolve bên trên
+  .catch((error) => console.log(error));
+
+/** Error */
+/** Always handle error when using promise. Luôn luôn handle error khi sử dụng promise */
+const promiseV2 = new Promise((resolve, reject) => {
+  reject(new Error("oops, something wrong!!!"));
+});
+promiseV2
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
