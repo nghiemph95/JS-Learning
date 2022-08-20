ef2d456e01920f6d25b9eb7e97bbed2ec1e71b2e
nghiemph95 <nghiemph95@gmail.com>
2022-08-20 21:34:40 +0700

    Updated: 20-08-2022 21:34:40

--- sml/async programing/11_JSON.js
+++ sml/async programing/11_JSON.js
@@ -1,1 +1,6 @@
 /** JSON: JavaScript Object Notation */
+/** chỉ cần biêt 2 hàm là stringtify và parse */
+
+/** Stringify */
+const student = { id: 1, name: "Easy Frontend", age: undefined };
+console.log(JSON.stringify(student));
