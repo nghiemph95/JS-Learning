905ba8ee3f81ea45c7804dfcc0f4594d91b125c8
nghiemph95 <nghiemph95@gmail.com>
2022-08-20 21:46:53 +0700

    Updated: 20-08-2022 21:46:53

---
+++ sml/async programing/debouceRepeatSetup.js
@@ -0,0 +1,24 @@
+/** Re-setup deboucne */
+
+/** setup */
+function debounce(callback, wait) {}
+
+function log() {}
+
+const showDebounce = debounce(log, wait);
+showDebounce();
+showDebounce();
+showDebounce();
+
+/** implement Debounce */
+function debounceSetup(callback, wait) {}
+
+function eventTrigger() {
+  console.log("The event has completed !");
+}
+
+const logDebounce = debounceSetup(eventTrigger, 3000);
+logDebounce();
+logDebounce();
+logDebounce();
+logDebounce();
