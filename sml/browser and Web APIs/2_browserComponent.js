c91b43387e94ec0736c4c772584d1d9865c6c29c
nghiemph95 <nghiemph95@gmail.com>
2022-08-23 14:15:58 +0700

    Updated: 23-08-2022 14:15:58

--- sml/browser and Web APIs/2_browserComponent.js
+++ sml/browser and Web APIs/2_browserComponent.js
@@ -10,4 +10,9 @@
 //4. Networking: Thực thi http request để lấy dữ liệu (HTTP, WebSocket, WebRTC(Streaming))
 //5. JS Engine: ECMAScript Engine (V8, SpiderMonkey...)
 //6. UI Backend: Giao diện mạc định của hệ điều hành
-//7. Data Storage:
+//7. Data Storage: Lưu trữ dữ liệu trên trình duyệt của mình
+
+/** Rendering Engine */
+// HTML -> HTML Parser -> DOM Tree
+// CSS -> CSS Parse -> Style Rules
+// (DOM Tree + Style Rules) -> Render Tree -> Paint & Layout -> Display
