d31a64cbcfc7564d46615c5535e06f90392a065a
nghiemph95 <nghiemph95@gmail.com>
2022-08-23 15:55:55 +0700

    Updated: 23-08-2022 15:55:55

--- sml/browser and Web APIs/3_webAPI.js
+++ sml/browser and Web APIs/3_webAPI.js
@@ -13,4 +13,10 @@
 /** Fetch API: Dùng để gửi request lên server */
 /** Geolocation API: Get location hiện tại */
 /** Drag and Drop API: Kéo thả */
-/** History API: Goback hoặc Forward/Previous */
+/** History API: Goback hoặc Forward/Previous. Ví dụ: history.back() / history.forward() */
+/** Server Send Event: Client sẽ lắng nghe sự kiện từ server, khi có sự kiện server sẽ báo về client */
+/** The WebSocket API: Giao tiếp 2 chiều giống như chatting giữ 2 người */
+/** Service Worker API: Offline, caching, thông báo khi chưa vào web */
+/** Storage: lưu trữ tạm thời trên browser. LocalStorage, SessionStorage */
+/** URL: Đường dẫn  */
+/** XMLHttpRequest: Gửi request lên server */
