a49563131014d97f60f27cf3eb7d9df4720e4323
nghiemph95 <nghiemph95@gmail.com>
2022-08-23 13:35:51 +0700

    Updated: 23-08-2022 13:35:51

--- sml/browser and Web APIs/1_browser_WebAPI.js
+++ sml/browser and Web APIs/1_browser_WebAPI.js
@@ -5,3 +5,9 @@
 // Internet: mạng lưới các máy tính kết nối với nhau
 
 /** cross browser testing */
+// Mỗi trình duyệt có 1 đặc thù và engine riêng, ví dụ
+// có những tính năng chạy đc trên này nhưng ko chạy được trên trình duyệt khác
+
+/** Vendor prefix */
+// -webkit-: chrome, safari
+// -moz-: mozila
