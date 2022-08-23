0a1f6df44493572b1b2d636c9ae91022643eb76e
nghiemph95 <nghiemph95@gmail.com>
2022-08-23 14:40:15 +0700

    Updated: 23-08-2022 14:40:15

---
+++ sml/browser and Web APIs/3_webAPI.js
@@ -0,0 +1,7 @@
+/** Web APIs - khi JSEngine khi thực thi code JS
+ * còn những thứ liên quan đến trình duyệt thì JSEngine ko biết
+ * tức là khi JS chạy nó đụng đến vấn đề của trình duyệt, nó sẽ
+ * đẩy nhiệm vụ qua WebAPIs
+ * Ví dụ: Khi muốn thực hiện lưu xún localStorage thì JSEngine ko biết,
+ * nên sẽ đẩy qua WebAPIs
+ */
