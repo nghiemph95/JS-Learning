3fa58afab1b19747db90bab83a169d5364bc007a
nghiemph95 <nghiemph95@gmail.com>
2022-08-23 14:01:18 +0700

    Updated: 23-08-2022 14:01:18

---
+++ sml/browser and Web APIs/2_browserComponent.js
@@ -0,0 +1,13 @@
+/** browser engine */
+// webkit - apple - Safari
+// Blink - Google - Chrome
+// Gecko - Mozilla - Firefox
+
+/** browser components */
+//1. User interface: Giao diện người dùng
+//2. Browser engine: Cung cấp phương thức/hàm để user interface gọi đến
+//3. Rendering Engine: Render nội dung HTML/CSS và các plugin
+//4. Networking: Thực thi http request để lấy dữ liệu (HTTP, WebSocket, WebRTC(Streaming))
+//5. JS Engine: ECMAScript Engine (V8, SpiderMonkey...)
+//6. UI Backend: Giao diện mạc định của hệ điều hành
+//7. Data Storage:
