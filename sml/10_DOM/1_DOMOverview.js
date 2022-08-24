4bb62b720f37e010233ba7dd640cf667b0b36808
nghiemph95 <nghiemph95@gmail.com>
2022-08-24 23:51:32 +0700

    Updated: 24-08-2022 23:51:32

--- sml/10_DOM/1_DOMOverview.js
+++ sml/10_DOM/1_DOMOverview.js
@@ -23,3 +23,20 @@
 /** DOM không chỉ có ở trình duyệt */
 /** BOM (Browser Object Model) là những object mà chỉ có trên trình duyệt.
  * Ví dụ: navigator, screen, location, frames, history, XMLHttpResquest */
+
+/** Những thư cơ bản khi làm việc với DOM : Tìm kiếm, duyệt, xử lý DOM */
+
+/** 4 bước thần thánh về DOM */
+//1. Query element
+const title = document.getElementById("pageTitle");
+
+//2. Check element exist
+if (title) {
+  //3. Manupulate the element
+  title.textContext = "test";
+
+  //4. Attach event listener: Click
+  title.addEventListener("click", () => {
+    title.style.color = "red";
+  });
+}
