619f479503ca048ae9fb83fde16e29d211389478
nghiemph95 <nghiemph95@gmail.com>
2022-08-24 23:42:40 +0700

    Updated: 24-08-2022 23:42:40

--- sml/10_DOM/1_DOMOverview.js
+++ sml/10_DOM/1_DOMOverview.js
@@ -1,4 +1,5 @@
-/** DOM: là 1 cái ánh xạ của file document, có thể là HTML hoặc XML */
+/** DOM: là cấu trúc dạng cây và là
+ * 1 cái ánh xạ của file document, có thể là HTML hoặc XML */
 
 /** Rendering Process
  * 1. File HTML sẽ build lên cây DOM (Document Object Model)
@@ -17,3 +18,7 @@
 /** Tại sao lại là cấu trúc cây: Bởi vì bên CSS sẽ có thuộc tính kế thừa
  * Ví dụ: thằng cha có font-size là 16px thì thằng con cũng được thừa hưởng
  */
+
+/** DOM không chỉ có ở trình duyệt */
+/** BOM (Browser Object Model) là những object mà chỉ có trên trình duyệt.
+ * Ví dụ: navigator, screen, location, frames, history, XMLHttpResquest */
