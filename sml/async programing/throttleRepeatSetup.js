69f6a7670d622e4f1cde755586505c50530c802e
nghiemph95 <nghiemph95@gmail.com>
2022-08-24 16:34:24 +0700

    Updated: 24-08-2022 16:34:23

--- sml/async programing/throttleRepeatSetup.js
+++ sml/async programing/throttleRepeatSetup.js
@@ -28,3 +28,34 @@ showThrottle();
 setTimeout(showThrottle, 2000);
 setTimeout(showThrottle, 3000);
 setTimeout(showThrottle, 4000);
+
+/** Setup 2nd throttle */
+function throttleRun(callback, wait) {
+  let flagThrottle = false;
+
+  return function () {
+    if (flagThrottle) return;
+
+    flagThrottle = true;
+    setTimeout(() => {
+      callback();
+      flagThrottle = false;
+    }, wait);
+  };
+}
+
+function showThrottleMessage() {
+  console.log("Throttle Show");
+}
+
+const funcThrottleRun = throttleRun(showThrottleMessage, 500);
+
+funcThrottleRun(); //turn on flag
+funcThrottleRun();
+funcThrottleRun();
+funcThrottleRun();
+funcThrottleRun();
+
+setTimeout(funcThrottleRun, 600);
+setTimeout(funcThrottleRun, 1000);
+setTimeout(funcThrottleRun, 1500);
