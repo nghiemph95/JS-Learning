eeaf350212c489671d0192484fa2cc98c39d6eb5
nghiemph95 <nghiemph95@gmail.com>
2022-08-24 23:20:50 +0700

    Updated: 24-08-2022 23:20:50

--- sml/10_DOM/1_DOMOverview.js
+++ sml/10_DOM/1_DOMOverview.js
@@ -1,1 +1,18 @@
 /** DOM: là 1 cái ánh xạ của file document, có thể là HTML hoặc XML */
+
+/** Rendering Process
+ * 1. File HTML sẽ build lên cây DOM (Document Object Model)
+ * 2. File CSS sẽ build lên cây CSSOM
+ * 3. Kết hợp 2 file này lại thành cây Render Tree -> Render lên màn hình
+ */
+
+/** DOM Construction
+ * 1. Bytes : 3C 42 6F 63 .....
+ * 2. Characters: <html><head>...</head><body></body>
+ * 3. Tokens: StartTag: html, StartTag: head.....
+ * 4. Nodes: html, head, meta, body, p, ...
+ * 5. DOM: DOM Tree (html -> head -> meta,link,body -> p,div,span)
+ */
+/** Tại sao lại là cấu trúc cây: Bởi vì bên CSS sẽ có thuộc tính kế thừa
+ * Ví dụ: thằng cha có font-size là 16px thì thằng con cũng được thừa hưởng
+ */
