d30fbd5c287884f89051706947f2b33460599733
nghiemph95 <nghiemph95@gmail.com>
2022-08-25 13:39:51 +0700

    Updated: 25-08-2022 13:39:51

--- sml/10_DOM/2_DOMStructure.js
+++ sml/10_DOM/2_DOMStructure.js
@@ -10,4 +10,22 @@ document.documentElement;
 document.head;
 document.body;
 
-/** Node data type */
+/** Node data type
+ * 1. EventTarget: root of the page, cung cấp hàm addEventListener và removeEventListener
+ * 2. Node: có nhiều loại node (text, element, comment...)
+ * 3. Element: có nhiều loại element (HTMLElement, SVGElement...)
+ * 4. HTMLElement: có nhiều thẻ gồm (HTMLInputElement, HTMLBodyElement, HTMLAnchorElement...)
+ */
+
+/** Tìm hiểu về Node
+ * Có rất nhiều ví dụ node.childNodes, node.firstChild, node.lastChild...
+ * Cung cấp các method, tiêu biểu là: Node.appendChild(childNode) và Node.cloneNode(deep)
+ * => tất cả các thẻ bên dưới node đều sẽ có những thuộc tính kế thừa từ node
+ */
+
+/** Node type
+ * 1. node.element_node (ELement node like <p> or <div>)
+ * 2. node.text_node (text)
+ * 3. node.comment_node (<!-- ... -->)
+ * 4. node.document_node (document note)
+ */
