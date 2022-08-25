7550850d08fdc92b04db84708febeabf200a2dc6
nghiemph95 <nghiemph95@gmail.com>
2022-08-25 13:22:59 +0700

    Updated: 25-08-2022 13:22:59

--- sml/10_DOM/2_DOMStructure.js
+++ sml/10_DOM/2_DOMStructure.js
@@ -1,2 +1,13 @@
 /** để truy cập đến node gốc trong cây DOM ta dùng document */
 document;
+window.document;
+global.document;
+
+// muốn lấy thẻ html ta dùng:
+document.documentElement;
+
+// ngoài ra có thể lấy thẻ head, body
+document.head;
+document.body;
+
+/** Node data type */
