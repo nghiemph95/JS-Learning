916792c0282f6e412b0a61b3a017e2d4052af877
nghiemph95 <nghiemph95@gmail.com>
2022-08-25 16:42:32 +0700

    Updated: 25-08-2022 16:42:32

---
+++ sml/10_DOM/js-dom/JS/addJStoHTML.js
@@ -0,0 +1,12 @@
+console.log('easfasdfsad');
+
+// function main() {
+//   let channelName = "Test";
+//   console.log("hello", channelName);
+// }
+
+//main - sử dụng IFEE
+(() => {
+  let channelName = 'Testing DOM';
+  console.log('hello', channelName);
+})();
