b9a97a5dd8253fb51ff232db3f3727c806c448a0
nghiemph95 <nghiemph95@gmail.com>
2022-08-25 16:43:28 +0700

    Updated: 25-08-2022 16:43:28

--- sml/10_DOM/js-dom/JS/addJStoHTML.js
+++ sml/10_DOM/js-dom/JS/addJStoHTML.js
@@ -9,4 +9,7 @@ console.log('easfasdfsad');
 (() => {
   let channelName = 'Testing DOM';
   console.log('hello', channelName);
+
+  const h1Element = document.getElementById('title');
+  console.log('h1', h1Element);
 })();
