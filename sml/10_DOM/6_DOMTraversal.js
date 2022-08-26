d1248c750c787e9345837451c9448b2fb00a6646
nghiemph95 <nghiemph95@gmail.com>
2022-08-26 12:13:55 +0700

    Updated: 26-08-2022 12:13:55

---
+++ sml/10_DOM/6_DOMTraversal.js
@@ -0,0 +1,62 @@
+/** Mục đích:
+ * 1. Duyệt node, duyệt parent
+ * 2. Phân biệt parentNode và parentElement
+ */
+
+/** Overview
+ * 1. Node: có nhiều loại Text, Element, Comment...
+ * 2. Element: html, svg element: div, p, section, h1,...
+ * 3. Children
+ */
+
+document; // root node
+
+document.documentElement; // html
+document.body; // body
+document.head; // head
+
+/** Duyệt node */
+Node.parentNode;
+Node.childNodes;
+Node.firstChild;
+Node.lastChild;
+Node.previousSibling;
+Node.nextSibling;
+
+/** Element ONLY */
+Node.parentElement;
+Element.children;
+Element.firstElementChild;
+Element.lastElementChild;
+Element.previousElementSibling;
+Element.nextElementSibling;
+
+/** Example */
+<html>
+  <head>
+    <title>Parcel Sandbox</title>
+    <meta charset="UTF-8" />
+  </head>
+  <body>
+    <h1>Easy Frontend</h1>
+    <p>Learning DOM is so easy! :P</p>
+    <script src="src/index.js"></script>
+  </body>
+</html>;
+
+console.log(document.body.parentNode); // html
+console.log(document.body.childNodes);
+// [text, comment, text, h1, text, p, text, script, text] (9 items 🤣 )
+console.log(document.body.firstChild); // text
+console.log(document.body.lastChild); // text
+console.log(document.body.previousSibling); // text
+console.log(document.body.nextSibling); // null
+
+/** Only elements traversal - Bộ element hữu dụng và đúng mong muốn hơn*/
+console.log(document.body.parentElement); // html
+console.log(document.body.children);
+// [h1, p, script] (only 3 items 😎 )
+console.log(document.body.firstElementChild); // h1
+console.log(document.body.lastElementChild); // script
+console.log(document.body.previousElementSibling); // head
+console.log(document.body.nextElementSibling); // null
