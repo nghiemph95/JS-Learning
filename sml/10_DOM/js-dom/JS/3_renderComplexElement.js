fa7a19d843053e9f8b1daa42ea23ced4abddf016
nghiemph95 <nghiemph95@gmail.com>
2022-08-29 13:48:55 +0700

    Updated: 29-08-2022 13:48:55

---
+++ sml/10_DOM/js-dom/JS/3_renderComplexElement.js
@@ -0,0 +1,47 @@
+function createTodoElement(todo) {
+  if (!todo) return;
+
+  const liElement = document.createElement('li');
+  liElement.textContent = todo.title;
+  // thêm id cho todo
+  liElement.dataset.id = todo.id;
+
+  return liElement;
+}
+
+function renderTodoList(todoList, ulElementId) {
+  if (!Array.isArray(todoList) || todoList.length === 0) return;
+
+  //find ul element
+  const ulElement = document.getElementById(ulElementId);
+  if (!ulElement) return;
+
+  // loop through todoList
+  for (const todo of todoList) {
+    // each todo -> create li element -> append to ul
+    const listElement = createTodoElement(todo);
+    ulElement.appendChild(listElement);
+  }
+}
+
+//main
+(() => {
+  const todoList1 = [
+    { id: 1, title: 'Learn JS' },
+    { id: 2, title: 'Learn ReactJS' },
+    { id: 3, title: 'Learn NextJS' },
+  ];
+
+  renderTodoList(todoList1, 'todoList1');
+
+  const todoList2 = [
+    { id: 1, title: 'Learn JS' },
+    { id: 2, title: 'Learn ReactJS' },
+    { id: 3, title: 'Learn NextJS' },
+    { id: 4, title: 'Learn Git' },
+  ];
+
+  renderTodoList(todoList2, 'todoList2');
+
+  //do something else
+})();
