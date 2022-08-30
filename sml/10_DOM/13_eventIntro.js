adc864fe64f7d37ec8dc181603b0a1a55e7d551d
nghiemph95 <nghiemph95@gmail.com>
2022-08-30 11:15:47 +0700

    Updated: 30-08-2022 11:15:47

--- sml/10_DOM/13_eventIntro.js
+++ sml/10_DOM/13_eventIntro.js
@@ -12,4 +12,38 @@
  * 5. Window events: beforeunload (khi load lại trang sẽ hỏi lại user có đồng ý load hay ko)
  */
 
-/**  */
+/** Có bao nhiêu cách gắn event
+ * 1. HTML onevent attribute: NOT RECOMMENDED
+ * 2. JS onevent properties: Nhược điểm chỉ adđ đc 1 handler
+ * 3. JS addEventListener: RECOMMENDED: Add được nhiều handler
+ */
+
+// HTML onevent attribute:
+<button onclick="alert('test')">Click me</button>;
+
+// JS onevent properties
+<button id="clickMeButton">Click me</button>;
+
+const clickMeButton = document.getElementById("clickMeButton");
+if (clickMeButton) {
+  // add only one handler
+  clickMeButton.onclick = function () {
+    alert("test");
+  };
+}
+
+//JS addEventListener
+<button id="clickMeButton">Click me</button>;
+
+const clickMeButton1 = document.getElementById("clickMeButton");
+if (clickMeButton1) {
+  clickMeButton1.addEventListener("click", () => {
+    alert("test");
+  });
+
+  clickMeButton1.addEventListener("click", () => {
+    alert("test11");
+  });
+}
+
+// Remove event
