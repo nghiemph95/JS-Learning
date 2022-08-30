34a05087943ce5a49cee312a8ddfaf5cc83f327a
nghiemph95 <nghiemph95@gmail.com>
2022-08-30 10:41:27 +0700

    Updated: 30-08-2022 10:41:27

---
+++ sml/10_DOM/13_eventIntro.js
@@ -0,0 +1,15 @@
+/** Mục đích:
+ * 1. Các loại event phổ biến
+ * 2. Có bao nhiêu cách gắn event
+ * 3. Remove event
+ */
+
+/** Các loại event phổ biến
+ * 1. Mouse event: Click, contextmenu, mouseover, mouseup, mousedown...
+ * 2. Keyboard events: keydown, keyup
+ * 3. Form element events: submit, focus
+ * 4. Document events: DOMContentLoad
+ * 5. Window events: beforeunload (khi load lại trang sẽ hỏi lại user có đồng ý load hay ko)
+ */
+
+/**  */
