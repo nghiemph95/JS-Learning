edb0ace163660881df168bacd8a9f78c1502a6fb
nghiemph95 <nghiemph95@gmail.com>
2022-08-30 10:01:43 +0700

    Updated: 30-08-2022 10:01:43

---
+++ sml/10_DOM/12_renderComplexTodoItem.js
@@ -0,0 +1,31 @@
+/** Khi ta muốn tạo nhiều thẻ html (li, ul)
+ * bằng cách nào đó có thể sử dụng document.createLement nhưng
+ * việc này tốn rất nhiều thời gian nếu buộc phải tạo nhiều element khác tương tự
+ */
+
+/** Issue with complex markup */
+<li>JavaScript</li>;
+
+function createTodoElement(todo) {
+  const liElement = document.createElement("li");
+  liElement.textContent = todo.title;
+
+  return liElement;
+}
+
+// nhưng khi gặp 1 markup phức tạp thì tạo từng cái sẽ làm mất rất nhiều thời gian
+<li>
+  <div class="todo alert alert-secondary" role="alert">
+    <div class="d-flex justify-content-between align-items-center">
+      <p class="todo__title mb-0">TODO TITLE</p>
+      <div class="todo__actions">
+        <button type="button" class="btn btn-success mark-as-done">
+          Finish
+        </button>
+        <button type="button" class="btn btn-danger remove">
+          Remove
+        </button>
+      </div>
+    </div>
+  </div>
+</li>;
