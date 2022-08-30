f4ec8e72272139cc2515a4c83694016ce47ba281
nghiemph95 <nghiemph95@gmail.com>
2022-08-30 10:05:35 +0700

    Updated: 30-08-2022 10:05:35

--- sml/10_DOM/12_renderComplexTodoItem.js
+++ sml/10_DOM/12_renderComplexTodoItem.js
@@ -29,3 +29,28 @@ function createTodoElement(todo) {
     </div>
   </div>
 </li>;
+
+/** Để giải quyết vấn đề : Sử dụng template
+ * 1. Sử dụng template
+ * 2. Clone template content
+ * 3. Thay đổi text content trong mỗi clone và gắn thêm event nếu cần
+ */
+
+// html file
+<template id="todoTemplate">
+  <li>
+    <div class="todo alert alert-secondary" role="alert">
+      <div class="d-flex justify-content-between align-items-center">
+        <p class="todo__title mb-0">TODO TITLE</p>
+        <div class="todo__actions">
+          <button type="button" class="btn btn-success mark-as-done">
+            Finish
+          </button>
+          <button type="button" class="btn btn-danger remove">
+            Remove
+          </button>
+        </div>
+      </div>
+    </div>
+  </li>
+</template>;
