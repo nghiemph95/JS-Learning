08412efc9202e83c9a5fdbf5636635715ef2fbb4
nghiemph95 <nghiemph95@gmail.com>
2022-09-03 16:06:07 +0700

    Updated: 03-09-2022 16:06:07

--- sml/10_DOM/17_browserAction.js
+++ sml/10_DOM/17_browserAction.js
@@ -26,3 +26,16 @@ form.addEventListener("submit", function (event) {
   // prevent from submit tigger browser reload
   event.preventDefault();
 });
+
+
+/** Lưu ý về passive event: 
+ * khi bật passive = true cho addEventListener thì handler sẽ ko gọi preventDefault()
+ * 
+ * ví dụ: đối với sự kiện touchmove trên thiết bị mobile, khi touch lên màn hình -> trigger
+ * scrolling -> trình duyệt sẽ đi check có thằng nào đang handle sự kiện
+ *  touchmove và nó có sử dụng preventDefault hay ko. Nếu có
+ * nó phải cancel cái scrooling -> gây ra tình trạng lác lác giựt giựt
+ * 
+ * => nên phải bật cái passive = true: nhằm ý nghĩa là ko có sự preventDefault,
+ * ko cần phải check nữa. Nên mặc định trên Firefox/Chrome sẽ bật passive = true cho sự kiện touchstart/touchmove
+ */
