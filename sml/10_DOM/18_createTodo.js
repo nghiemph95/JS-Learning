5928ba72b20982c0cdc88e4fa228f12aea17f86d
nghiemph95 <nghiemph95@gmail.com>
2022-09-05 12:06:02 +0700

    Updated: 05-09-2022 12:06:02

---
+++ sml/10_DOM/18_createTodo.js
@@ -0,0 +1,6 @@
+/** Tạo mới 1 cái todo để append vào list hiện tại:
+ * 1. Tạo 1 cái form có ô input để user gõ và submit
+ * 2. Lưu giá trị vào localStorage
+ * 3. Gắn thêm 1 phần tử todo mới vào DOM của mình
+ * 4. Reset todo form để 
+ */
