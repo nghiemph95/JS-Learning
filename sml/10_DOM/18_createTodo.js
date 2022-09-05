c26f26b5f9c96aee1c41bbb105deab076e31c6e9
nghiemph95 <nghiemph95@gmail.com>
2022-09-05 16:01:21 +0700

    Updated: 05-09-2022 16:01:21

--- sml/10_DOM/18_createTodo.js
+++ sml/10_DOM/18_createTodo.js
@@ -2,5 +2,5 @@
  * 1. Tạo 1 cái form có ô input để user gõ và submit
  * 2. Lưu giá trị vào localStorage
  * 3. Gắn thêm 1 phần tử todo mới vào DOM của mình
- * 4. Reset todo form để 
+ * 4. Reset todo form để bắt đầu 1 cái todo mới
  */
