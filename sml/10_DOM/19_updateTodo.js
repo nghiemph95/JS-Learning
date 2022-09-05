f3f45ed0844f3602822d26d30de3677cdb8ed7f7
nghiemph95 <nghiemph95@gmail.com>
2022-09-05 16:25:48 +0700

    Updated: 05-09-2022 16:25:48

---
+++ sml/10_DOM/19_updateTodo.js
@@ -0,0 +1,9 @@
+/** update todo
+ * 1. Click Edit todo Form, nút edit
+ * 2. Lấy thông tin todoitme hiển thị lên todoForm
+ * 3. Sửa
+ * 4. Submit
+ * 5. Save vô localstorage
+ * 6. Update thẻ li tương ứng
+ * 7. Reset todo form để sẵn sàng cho lượt típ theo
+ */
