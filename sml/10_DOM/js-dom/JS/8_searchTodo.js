5e17e052c235f75eabb9325d1af8926a9d3ffc10
nghiemph95 <nghiemph95@gmail.com>
2022-09-07 22:46:01 +0700

    Updated: 07-09-2022 22:46:01

--- sml/10_DOM/js-dom/JS/8_searchTodo.js
+++ sml/10_DOM/js-dom/JS/8_searchTodo.js
@@ -1,3 +1,23 @@
+// để duyệt qua các li thì phải đi tìm nó
+function getAllTodoElement() {
+  return document.querySelectorAll('#todoList > li');
+}
+
+// duyệt qua mảng li xem có match vs searchTerm ko
+function isMatch(liElement, searchTerm) {}
+
+// hàm search
+function searchTodo(searchTerm) {
+  const todoElementList = getAllTodoElement();
+
+  for (const todoElement of todoElementList) {
+    console.log(todoElement);
+  }
+  // nếu searchTerm ko có giá trị = empty thì show tất cả
+
+  // nếu searchTerm != empty -> filter todo
+}
+
 function initSearchInput() {
   // find search term input
   const searchInput = document.getElementById('searchTerm');
@@ -6,6 +26,7 @@ function initSearchInput() {
   // gắn sự kiên cho searchInput
   searchInput.addEventListener('input', () => {
     console.log('input', searchInput.value);
+    searchTodo(searchInput.value);
   });
 }
 
