e6ec3802303b3b87a35fc951a6aff80b532e9d81
nghiemph95 <nghiemph95@gmail.com>
2022-09-09 09:53:55 +0700

    Updated: 09-09-2022 09:53:55

--- sml/10_DOM/21_filterTodo.js
+++ sml/10_DOM/21_filterTodo.js
@@ -1,2 +1,28 @@
 /** filter todo */
 
+function filterTodo(filterStatus) {
+  const todoElementList = document.querySelectorAll("#todoList > li");
+
+  for (const todoElement of todoElementList) {
+    const isShow =
+      todoElement === "all" ||
+      todoElement.dataset.status === filterStatus;
+
+    todoElement.hidden = !isShow;
+  }
+}
+
+function initFilterStatus() {
+  // find select
+  const filterStatusSelect = document.getElementById("filterStatus");
+  if (!filterStatusSelect) return;
+  // attach event
+  filterStatusSelect.addEventListener("change", () => {
+    filterTodo(filterStatusSelect.value);
+  });
+}
+
+//main
+(() => {
+  initFilterStatus();
+})();
