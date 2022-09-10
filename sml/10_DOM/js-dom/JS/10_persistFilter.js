f40a35261b6921acacaf36b7d496173a170e0d82
nghiemph95 <nghiemph95@gmail.com>
2022-09-10 23:07:09 +0700

    Updated: 10-09-2022 23:07:09

--- sml/10_DOM/js-dom/JS/10_persistFilter.js
+++ sml/10_DOM/js-dom/JS/10_persistFilter.js
@@ -3,8 +3,11 @@ function getAllTodoElement() {
   return document.querySelectorAll('#todoList > li');
 }
 
-// duyệt qua mảng li xem có match vs searchTerm ko
-function isMatch(liElement, searchTerm) {
+function isMatchStatus(liElement, filterStatus) {
+  return filterStatus === 'all' || filterStatus === todoElement.dataset.status;
+}
+
+function isMatchSearch(liElement, searchTerm) {
   if (!liElement) return false;
   // nếu searchTerm ko có giá trị = empty thì show tất cả
   if (searchTerm === '') return true;
@@ -16,16 +19,23 @@ function isMatch(liElement, searchTerm) {
   return titleElement.textContent.toLowerCase().includes(searchTerm.toLowerCase());
 }
 
-// hàm search
-function searchTodo(searchTerm) {
-  const todoElementList = getAllTodoElement();
-
-  for (const todoElement of todoElementList) {
-    const needToShow = isMatch(todoElement, searchTerm);
-
-    todoElement.hidden = !needToShow;
-  }
+function isMatch(liElement, params) {
+  return (
+    isMatchSearch(liElement, params.get('searchTerm')) &&
+    isMatchStatus(liElement, params.get('status'))
+  );
 }
+
+// hàm search
+// function searchTodo(searchTerm) {
+//   const todoElementList = getAllTodoElement();
+
+//   for (const todoElement of todoElementList) {
+//     const needToShow = isMatch(todoElement, searchTerm);
+
+//     todoElement.hidden = !needToShow;
+//   }
+// }
 
 function initSearchInput() {
   // find search term input
@@ -34,19 +44,34 @@ function initSearchInput() {
 
   // gắn sự kiên cho searchInput
   searchInput.addEventListener('input', () => {
-    searchTodo(searchInput.value);
+    // searchTodo(searchInput.value);
+    handleFilterChange('searchTerm', searchInput.value);
   });
 }
 
-function filterTodo(filterStatus) {
-  const todoElementList = getAllTodoElement();
+function handleFilterChange(filterName, filterValue) {
+  //update query params
+  const url = new URL(window.location);
+  url.searchParams.set(filterName, filterValue);
+  history.pushState({}, '', url);
 
+  const todoElementList = getAllTodoElement();
   for (const todoElement of todoElementList) {
-    const needToShow = filterStatus === 'all' || filterStatus === todoElement.dataset.status;
+    const needToShow = isMatch(todoElement, url.searchParams);
 
     todoElement.hidden = !needToShow;
   }
 }
+
+// function filterTodo(filterStatus) {
+//   const todoElementList = getAllTodoElement();
+
+//   for (const todoElement of todoElementList) {
+//     const needToShow = filterStatus === 'all' || filterStatus === todoElement.dataset.status;
+
+//     todoElement.hidden = !needToShow;
+//   }
+// }
 
 function initFilterStatus() {
   // find select
@@ -55,7 +80,8 @@ function initFilterStatus() {
 
   // attach event change
   filterStatusSelect.addEventListener('change', () => {
-    filterTodo(filterStatusSelect.value);
+    // filterTodo(filterStatusSelect.value);
+    handleFilterChange('status', filterStatusSelect.value);
   });
 }
 
