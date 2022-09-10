a2ee51897777e926ed9750fcb548456e482356aa
nghiemph95 <nghiemph95@gmail.com>
2022-09-10 21:58:02 +0700

    Updated: 10-09-2022 21:58:01

---
+++ sml/10_DOM/js-dom/JS/10_persistFilter.js
@@ -0,0 +1,67 @@
+// để duyệt qua các li thì phải đi tìm nó
+function getAllTodoElement() {
+  return document.querySelectorAll('#todoList > li');
+}
+
+// duyệt qua mảng li xem có match vs searchTerm ko
+function isMatch(liElement, searchTerm) {
+  if (!liElement) return false;
+  // nếu searchTerm ko có giá trị = empty thì show tất cả
+  if (searchTerm === '') return true;
+
+  // nếu searchTerm != empty -> filter todo
+  const titleElement = liElement.querySelector('p.todo__title');
+  if (!titleElement) return false;
+
+  return titleElement.textContent.toLowerCase().includes(searchTerm.toLowerCase());
+}
+
+// hàm search
+function searchTodo(searchTerm) {
+  const todoElementList = getAllTodoElement();
+
+  for (const todoElement of todoElementList) {
+    const needToShow = isMatch(todoElement, searchTerm);
+
+    todoElement.hidden = !needToShow;
+  }
+}
+
+function initSearchInput() {
+  // find search term input
+  const searchInput = document.getElementById('searchTerm');
+  if (!searchInput) return;
+
+  // gắn sự kiên cho searchInput
+  searchInput.addEventListener('input', () => {
+    searchTodo(searchInput.value);
+  });
+}
+
+function filterTodo(filterStatus) {
+  const todoElementList = getAllTodoElement();
+
+  for (const todoElement of todoElementList) {
+    const needToShow = filterStatus === 'all' || todoElement.dataset.status === filterStatus;
+
+    todoElement.hidden = !needToShow;
+  }
+}
+
+function initFilterStatus() {
+  // find select
+  const filterStatusSelect = document.getElementById('filterStatus');
+  if (!filterStatusSelect) return;
+
+  // attach event change
+  filterStatusSelect.addEventListener('change', () => {
+    console.log('status change', filterStatusSelect.value);
+    filterTodo(filterStatusSelect.value);
+  });
+}
+
+//main
+(() => {
+  initSearchInput();
+  initFilterStatus();
+})();
