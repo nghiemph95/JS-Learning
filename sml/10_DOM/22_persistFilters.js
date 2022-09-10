ea92ea0ab0ae32a38c5aa1f7eb9d90f8b48d7d35
nghiemph95 <nghiemph95@gmail.com>
2022-09-10 22:17:44 +0700

    Updated: 10-09-2022 22:17:44

---
+++ sml/10_DOM/22_persistFilters.js
@@ -0,0 +1,6 @@
+/** Search được và filter được nhưng chưa kết hợp được 
+ * 1. Khi có filter change
+ * 2. Update query param sử dụng history.pushState (không reload lại trình duyệt mà chỉ cập nhật lại queryParam)
+ * 3. Sử dụng param để check li
+ * 
+*/
