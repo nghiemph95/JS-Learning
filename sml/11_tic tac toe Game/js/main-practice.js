8c6618830e4c0ca9e43ef5e25237f35672e20186
nghiemph95 <nghiemph95@gmail.com>
2022-09-15 15:25:58 +0700

    Updated: 15-09-2022 15:25:58

--- sml/11_tic tac toe Game/js/main-practice.js
+++ sml/11_tic tac toe Game/js/main-practice.js
@@ -1,5 +1,5 @@
 import { GAME_STATUS, TURN } from "./constants.js";
-import { getCellElementList } from "./selectors.js";
+import { getCellElementList, getCurrentTurnElement } from "./selectors.js";
 
 /** Global variable */
 let currentTurn = TURN.CROSS;
@@ -20,12 +20,34 @@ let cellValues = new Array(9).fill("");
  * 4. On replay button -> reset game to play again.
  */
 
+//Toggle turn
+function toggleTurn() {
+  //toggle turn
+  currentTurn = currentTurn === TURN.CIRCLE ? TURN.CROSS : TURN.CIRCLE;
+
+  //update turn X/O to DOM element
+  const currentTurnElement = getCurrentTurnElement();
+  if (currentTurnElement) {
+    currentTurnElement.classList.remove(TURN.CIRCLE, TURN.CROSS);
+    currentTurnElement.classList.add(currentTurn);
+  }
+}
+
 //Handle cell clicks
 function handleCellClick(cell, index) {
   console.log("click", cell, index);
 
+  // check cell is already cross/circle
+  const isClicked =
+    cell.classList.contains(TURN.CIRCLE) || cell.classList.contains(TURN.CROSS);
+
+  if (isClicked) return;
+
   // add selected cell
   cell.classList.add(currentTurn);
+
+  // toggle current turn
+  toggleTurn();
 }
 
 //Bind click event for all cells (liElements)
