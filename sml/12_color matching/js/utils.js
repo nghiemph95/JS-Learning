a3b047a461d90bf5c1b3adc478e446997ca1ea86
nghiemph95 <nghiemph95@gmail.com>
2022-09-19 21:28:05 +0700

    Updated: 19-09-2022 21:28:05

--- sml/12_color matching/js/utils.js
+++ sml/12_color matching/js/utils.js
@@ -1,4 +1,10 @@
 export const getRandomColorPairs = (count) => {
   // receive count --> return count * 2 random colors
   // using lib: https://github.com/davidmerfield/randomColor
+
+  const colorList = []
+
+  const hueList = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'monochrome']
+
+  return colorList
 }
