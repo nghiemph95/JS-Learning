6ae9c228ed4a71b948c51e5daac0fb8dd92a9fcf
nghiemph95 <nghiemph95@gmail.com>
2022-09-19 23:15:05 +0700

    Updated: 19-09-2022 23:15:05

--- sml/12_color matching/js/main.js
+++ sml/12_color matching/js/main.js
@@ -1,9 +1,9 @@
-import { GAME_STATUS } from './constants.js'
-import { getRandomColorPairs } from './utils.js'
+import { GAME_STATUS, PAIRS_COUNT } from './constants.js';
+import { getRandomColorPairs } from './utils.js';
 
 // Global variables
-let selections = []
-let gameState = GAME_STATUS.PLAYING
+let selections = [];
+let gameState = GAME_STATUS.PLAYING;
 
 // TODOs
 // 1. Generating colors using https://github.com/davidmerfield/randomColor
@@ -11,4 +11,20 @@ let gameState = GAME_STATUS.PLAYING
 // 3. Check win logic
 // 4. Add timer
 // 5. Handle replay click
-console.log(getRandomColorPairs(4))
+console.log(getRandomColorPairs(16));
+
+function initColors() {
+  // random 8 colors
+  const colorList = getRandomColorPairs(PAIRS_COUNT); // 16 colors
+  // bind to li > div.overlay
+  const liList = getColorElementList();
+  liList.forEach((liElement, index) => {
+    const overlayElement = liElement.querySelector('.overlay');
+    overlayElement.style.backgroundColor = colorList[index];
+  });
+}
+
+//main
+(() => {
+  initColors();
+})();
