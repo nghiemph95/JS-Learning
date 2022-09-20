a4d5d7a1e3c87c2f207cce15334c5650711c034f
nghiemph95 <nghiemph95@gmail.com>
2022-09-20 15:29:16 +0700

    Updated: 20-09-2022 15:29:15

--- sml/12_color matching/js/main.js
+++ sml/12_color matching/js/main.js
@@ -19,6 +19,29 @@ function handleColorClick(liElement) {
   if (!liElement) return
 
   liElement.classList.add('active')
+
+  // sau mỗi lần click lưu các thông tin vào 1 mảng tạm
+  selections.push(liElement)
+  // nếu click 1 lần thì chưa làm gì cả, chừng nào click 2 lần thì mới tiếp tục check
+  if (selections.length < 2) return
+
+  // kiểm tra sau 2 lần click, 2 màu sắc có giống hoặc khác nhau ?
+  const firstColor = selections[0].dataset.color
+  const secondColor = selections[1].dataset.color
+  const isMatch = firstColor === secondColor
+
+  if (isMatch) {
+    // kiểm tra đã win chưa, đã đủ 16 ô hay chưa
+
+    return
+  }
+
+  // trường hợp không match
+  // xóa bỏ class-active cho 2 li-element đang đc click
+  selections[0].classList.remove('active')
+  selections[1].classList.remove('active')
+  // reset lại mảng rỗng cho selections cho lượt kế tiếp
+  selections = []
 }
 
 function initColors() {
@@ -29,6 +52,9 @@ function initColors() {
   const liList = getColorElementList() // 16 ô liElement
 
   liList.forEach((liElement, index) => {
+    //tạo data color cho từng thẻ liElement
+    liElement.dataset.color = colorList[index]
+    // gắn màu cho từng thẻ liElement
     const overlayElement = liElement.querySelector('.overlay')
     if (overlayElement) overlayElement.style.backgroundColor = colorList[index]
   })
@@ -39,6 +65,7 @@ function attachEventForColorList() {
 
   // sử dụng kỹ thuật event delegation để add sự kiên click cho thằng cha
   ulElement.addEventListener('click', (event) => {
+    if (event.target.tagName !== 'LI') return
     handleColorClick(event.target)
   })
 }
