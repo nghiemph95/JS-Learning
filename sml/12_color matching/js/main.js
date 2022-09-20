62ea0035d7fcad60c5d9585ece204e039ceb71bb
nghiemph95 <nghiemph95@gmail.com>
2022-09-20 11:21:58 +0700

    Updated: 20-09-2022 11:21:58

--- sml/12_color matching/js/main.js
+++ sml/12_color matching/js/main.js
@@ -1,10 +1,10 @@
-import { GAME_STATUS, PAIRS_COUNT } from './constants.js';
-import { getColorListElement, getColorElementList } from './selectors.js';
-import { getRandomColorPairs } from './utils.js';
+import { GAME_STATUS, PAIRS_COUNT } from './constants.js'
+import { getColorListElement, getColorElementList } from './selectors.js'
+import { getRandomColorPairs } from './utils.js'
 
 // Global variables
-let selections = [];
-let gameState = GAME_STATUS.PLAYING;
+let selections = []
+let gameState = GAME_STATUS.PLAYING
 
 // TODOs
 // 1. Generating colors using https://github.com/davidmerfield/randomColor
@@ -12,32 +12,32 @@ let gameState = GAME_STATUS.PLAYING;
 // 3. Check win logic
 // 4. Add timer
 // 5. Handle replay click
-console.log(getRandomColorPairs(PAIRS_COUNT));
+console.log(getRandomColorPairs(PAIRS_COUNT))
 
 function initColors() {
   // random 8 colors
-  const colorList = getRandomColorPairs(PAIRS_COUNT); // 16 colors
+  const colorList = getRandomColorPairs(PAIRS_COUNT) // 16 colors
 
   // bind to li > div.overlay
-  const liList = getColorElementList(); // 16 liElement
+  const liList = getColorElementList() // 16 liElement
 
   liList.forEach((liElement, index) => {
-    const overlayElement = liElement.querySelector('.overlay');
-    if (overlayElement) overlayElement.style.backgroundColor = colorList[index];
-  });
+    const overlayElement = liElement.querySelector('.overlay')
+    if (overlayElement) overlayElement.style.backgroundColor = colorList[index]
+  })
 }
 
 function attachEventForColorList() {
-  const ulElement = getColorListElement();
+  const ulElement = getColorListElement()
 
   ulElement.addEventListener('click', (event) => {
-    console.log(event.target);
-  });
+    console.log(event.target)
+  })
 }
 
 //main
-(() => {
-  initColors();
+;(() => {
+  initColors()
 
-    attachEventForColorList();
-})();
+  attachEventForColorList()
+})()
