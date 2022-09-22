6306a4d4da87e4f51a56a23f2b5d8c9d6a75502f
nghiemph95 <nghiemph95@gmail.com>
2022-09-22 09:54:09 +0700

    Updated: 22-09-2022 09:54:09

--- sml/12_color matching/js/selectors.js
+++ sml/12_color matching/js/selectors.js
@@ -1,26 +1,26 @@
 // lấy ra thẻ li
 export function getColorElementList() {
-  return document.querySelectorAll('#colorList > li')
+  return document.querySelectorAll('#colorList > li');
 }
 
 export function getTimerElement() {
-  return document.querySelector('.game .game__timer')
+  return document.querySelector('.game .game__timer');
 }
 
 export function getPlayAgainButton() {
-  return document.querySelector('.game .game__button')
+  return document.querySelector('.game .game__button');
 }
 
 export function getColorBackground() {
-  return document.querySelector('.color-background')
+  return document.querySelector('.color-background');
 }
 
 // lấy ra thẻ ul
 export function getColorListElement() {
-  return document.getElementById('colorList')
+  return document.getElementById('colorList');
 }
 
 // lấy những thẻ li ko có class là active
 export function getInActiveColorList() {
-  return document.querySelectorAll('#colorList > li:not(.active)')
+  return document.querySelectorAll('#colorList > li:not(.active)');
 }
--- sml/12_color matching/js/utils.js
+++ sml/12_color matching/js/utils.js
@@ -1,14 +1,14 @@
-import { getPlayAgainButton, getTimerElement } from './selectors.js'
+import { getPlayAgainButton, getTimerElement } from './selectors.js';
 
 function shuffle(arr) {
-  if (!Array.isArray(arr) || arr.length <= 2) return arr
+  if (!Array.isArray(arr) || arr.length <= 2) return arr;
 
   for (let i = arr.length - 1; i > 1; i--) {
-    const j = Math.floor(Math.random() * i)
+    const j = Math.floor(Math.random() * i);
 
-    let temp = arr[i]
-    arr[i] = arr[j]
-    arr[j] = temp
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
   }
 }
 
@@ -16,72 +16,71 @@ export const getRandomColorPairs = (count) => {
   // receive count --> return count * 2 random colors
   // using lib: https://github.com/davidmerfield/randomColor
 
-  const colorList = []
+  const colorList = [];
 
-  const hueList = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'monochrome']
+  const hueList = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'monochrome'];
 
   //random "count" color
   for (let i = 0; i < count; i++) {
     const color = window.randomColor({
       luminosity: 'dark',
       hue: hueList[i % hueList.length],
-    })
+    });
 
-    colorList.push(color)
+    colorList.push(color);
   }
 
   // double current color list
-  const fullColorList = [...colorList, ...colorList]
+  const fullColorList = [...colorList, ...colorList];
   // shuffle
-  shuffle(fullColorList)
+  shuffle(fullColorList);
 
-  return fullColorList
-}
+  return fullColorList;
+};
 
 export function showPlayAgainButton() {
-  const playAgainButton = getPlayAgainButton()
-  if (playAgainButton) playAgainButton.classList.add('show')
+  const playAgainButton = getPlayAgainButton();
+  if (playAgainButton) playAgainButton.classList.add('show');
 }
 
 export function hidePlayAgainButton() {
-  const playAgainButton = getPlayAgainButton()
-  if (playAgainButton) playAgainButton.classList.remove('show')
+  const playAgainButton = getPlayAgainButton();
+  if (playAgainButton) playAgainButton.classList.remove('show');
 }
 
 export function setTimerText(text) {
-  const timerElement = getTimerElement()
-  if (timerElement) timerElement.textContent = text
+  const timerElement = getTimerElement();
+  if (timerElement) timerElement.textContent = text;
 }
 
 // hàm createTimer
 export function createTimer({ seconds, onChange, onFinish }) {
-  let intervalId = null
+  let intervalId = null;
 
   // sử dụng closure
   function start() {
     // trường hợp cho chắc thì trước khi chạy, cần phải clear trước
-    clear()
+    clear();
     // để đảm bảo không thay đổi tham số đầu vào, mình cần gán 1 giá trị để hàm có thể chạy từ đó
-    let currentSecond = seconds
+    let currentSecond = seconds;
     // khi start cứ sau 1s nó sẽ gọi hàm setInterval
     intervalId = setInterval(() => {
-      onChange?.(currentSecond)
+      onChange?.(currentSecond);
 
-      currentSecond--
+      currentSecond--;
       if (currentSecond < 0) {
-        clear()
-        onFinish?.()
+        clear();
+        onFinish?.();
       }
-    }, 1000)
+    }, 1000);
   }
 
-  
   function clear() {
-    clearInterval(intervalId)
+    clearInterval(intervalId);
   }
 
   return {
     start,
     clear,
-  }
+  };
 }
