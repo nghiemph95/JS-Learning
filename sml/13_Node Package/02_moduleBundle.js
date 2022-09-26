52b4e797dfe13ac3fcfba45d4c709a8f10e0f266
nghiemph95 <nghiemph95@gmail.com>
2022-09-26 21:52:12 +0700

    Updated: 26-09-2022 21:52:12

--- sml/13_Node Package/02_moduleBundle.js
+++ sml/13_Node Package/02_moduleBundle.js
@@ -1,1 +1,20 @@
-/** Module */
+/** Module:
+ * Nếu các file JS import lần nhau thì nó là 1 module
+ * ESM: ECMAScript Modules
+ */
+
+// Minify: Code sẽ biến thành 1 hàng, bỏ qua khoảng trắng...
+// Uglify: Tên biến sẽ đc tối giản thành các (a,b,c...), làm giảm đi các bộ nhớ
+
+/** Task Runner: Các chương trình giúp run các task một cách tự động
+ * dựa trên một hoặc nhiều điều kiện nào đó hoặc giúp run thủ công
+ */
+
+/** Module Bundle: là Công cụ giúp gộp các JS modules thành 1 tệp duy nhất và
+ * thường đc chạy trong trình duyệt
+ *
+ * Module bundle: Webpack, Rollup, Parcel....
+ * - Webpack: công cụ sử dụng để quản lý các module JS, nó đóng gói tất cả các mã nguồn
+ * của chương trình cũng như CSS, hình ảnh, font,... .Assets chính là tên để gọi những thứ
+ * đóng gói này và chúng đc webpack đóng gói thành file hoặc 1 vài file.
+ */
