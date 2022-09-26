de3e32250d922a846a8f14c7f7a78e0d3340b8fc
nghiemph95 <nghiemph95@gmail.com>
2022-09-26 22:02:36 +0700

    Updated: 26-09-2022 22:02:36

--- sml/13_Node Package/02_moduleBundle.js
+++ sml/13_Node Package/02_moduleBundle.js
@@ -18,3 +18,27 @@
  * của chương trình cũng như CSS, hình ảnh, font,... .Assets chính là tên để gọi những thứ
  * đóng gói này và chúng đc webpack đóng gói thành file hoặc 1 vài file.
  */
+
+/**
+ * Hiện nay với sự phát triển như vũ bão của Javascript đặc biệt
+ * là các đoạn mã code được viết sẵn như ReactJS, AngularJS,... giúp giảm thiểu code và
+ * các CSS giúp nâng cao trải nghiệm người dùng khiến cho bộ nhớ và
+ * các module JS trong ứng dụng không thể kiểm soát và gây tình trạng phình website.
+ * Lúc này, vấn đề xảy ra là sắp xếp các mã nguồn như thế nào để dễ quản lý và truy xuất dữ liệu.
+ * Chính vì vậy, việc sử dụng Webpack nhằm giúp cải thiện tình trạng bừa bãi và
+ * quá tải là một sự lựa chọn đúng đắn.
+ * Ngoài ra, Webpack còn có thể thực hiện được nhiều hoạt động khác nhau như:
+ * - Giúp đóng gói resources.
+ * - Theo dõi những thay đổi và chạy lại các task.
+ * - Có thể thực hiện biên dịch Babel chuyển sang ES5, cho phép người dùng sử dụng các 
+ *tính năng JavaScript mới nhất mà không phải lo về hỗ trợ trình duyệt.
+ * - Có thể dịch từ CoffeeScript sang JavaScript.
+ * - Có thể chuyển đổi ảnh inline sang data URIs.
+ * - Cho phép người dùng sử dụng require() cho các file CSS.
+ * - Có thể chạy một web server dùng cho development.
+ * - Có thể xử lý hot module replacement (thay vì mỗi lần thay đổi code thì nó sẽ làm full reload thì
+ * bây giờ nó chỉ replay cái chỗ thay đổi để reload chứ ko reload toàn bộ).
+ * - Có thể tách các file output thành nhiều file khác nhau để tránh tải file js lớn 
+ * trong lần đầu tải trang.
+ * - Có thực hiện tree shaking(có thể remove dead code, không còn sử dụng).
+ */
