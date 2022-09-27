06a0cba30fa9d8e73e133d8e03b423ddbeef4667
nghiemph95 <nghiemph95@gmail.com>
2022-09-27 09:57:24 +0700

    Updated: 27-09-2022 09:57:24

--- sml/13_Node Package/01_node_package_manager.js
+++ sml/13_Node Package/01_node_package_manager.js
@@ -4,7 +4,7 @@
 
 /**
  * file package.json: file chứa những config trong dự án
- * devDependencies: những thư viện/package chỉ dùng cho local developmen
+ * devDependencies: những thư viện/package chỉ dùng cho local development
  * dependencies: khi build lên prod thì nó sẽ lấy những thư viện trong dependencies
  *
  * script: những lệnh để khi thực thi nó sẽ chạy những dòng đã được viết sẵn trong script
