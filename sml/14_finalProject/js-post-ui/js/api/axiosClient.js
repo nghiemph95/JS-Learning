cb8ccd0dac2f8e07589e023686e94146ae8a6864
nghiemph95 <nghiemph95@gmail.com>
2022-09-28 22:27:47 +0700

    Updated: 28-09-2022 22:27:47

--- sml/14_finalProject/js-post-ui/js/api/axiosClient.js
+++ sml/14_finalProject/js-post-ui/js/api/axiosClient.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "https://js-post-api.herokuapp/api",
+  baseURL: "https://js-post-api.herokuapp.com/api",
   headers: {
     "Content-Type": "application/json",
   },
--- sml/14_finalProject/js-post-ui/js/main.js
+++ sml/14_finalProject/js-post-ui/js/main.js
@@ -1,9 +1,15 @@
 import axiosClient from "./api/axiosClient";
+import postApi from "./api/postApi";
 
 console.log("Hello !");
 
 async function main() {
-  const response = await axiosClient.get("/posts");
+  //   const response = await axiosClient.get("/posts");
+  const queryParams = {
+    _page: 1,
+    _limit: 5,
+  };
+  const response = await postApi.getAll(queryParams);
   console.log(response);
 }
 
