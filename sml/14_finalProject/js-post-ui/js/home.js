ee7034658835adde9e2a024331c4b41c2b7be33e
nghiemph95 <nghiemph95@gmail.com>
2022-09-30 00:06:31 +0700

    Updated: 30-09-2022 00:06:31

--- sml/14_finalProject/js-post-ui/js/home.js
+++ sml/14_finalProject/js-post-ui/js/home.js
@@ -1,7 +1,30 @@
 import postApi from "./api/postApi";
 
+function createPostElement(post) {
+  if (!post) return;
+
+  // find and clone template
+  const postTemplate = document.getElementById("postTemplate");
+  if (!postTemplate) return;
+
+  const liElement = postTemplate.content.firstElementChild.cloneNode(true);
+  // update template (title, description, author, thumbnail)
+
+  // attach event
+}
+
 function renderPostList(postList) {
   console.log(postList);
+
+  if (!Array.isArray(postList) || postList.length === 0) return;
+
+  const ulElement = document.getElementById("postList");
+  if (!ulElement) return;
+
+  postList.forEach((post) => {
+    const liElement = createPostElement(post);
+    ulElement.appendChild(liElement);
+  });
 }
 
 (async () => {
