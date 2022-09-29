86529802d159ceac7d2ff84311a3abb7a3cdab5f
nghiemph95 <nghiemph95@gmail.com>
2022-09-29 15:56:58 +0700

    Updated: 29-09-2022 15:56:58

--- sml/14_finalProject/js-post-ui/js/api/axiosClient.js
+++ sml/14_finalProject/js-post-ui/js/api/axiosClient.js
@@ -5,7 +5,7 @@ const axiosClient = axios.create({
   baseURL: "https://js-post-api.herokuapp.com/api",
   headers: {
     //trường hợp sử dụng thông tin có body(POST, PUT, PATCH), phải chỉ định content type là gì
-    "Content-Type": "application/json", // content type phải viết đúng để nó có thể đọc được payload tương ứng.  
+    "Content-Type": "application/json", // content type phải viết đúng để nó có thể đọc được payload tương ứng.
   },
 });
 
