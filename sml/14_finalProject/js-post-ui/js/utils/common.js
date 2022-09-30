ced14bf66d24c8a0a672775a2b0a0701241dfe1d
nghiemph95 <nghiemph95@gmail.com>
2022-09-30 14:44:07 +0700

    Updated: 30-09-2022 14:44:07

--- sml/14_finalProject/js-post-ui/js/utils/common.js
+++ sml/14_finalProject/js-post-ui/js/utils/common.js
@@ -9,5 +9,11 @@ export function setImageContent(parent, selector, imageUrl) {
   if (!parent) return;
 
   const element = parent.querySelector(selector);
-  if (element) element.src = imageUrl;
+  if (element) {
+    element.src = imageUrl;
+
+    element.addEventListener("error", () => {
+      element.src = "https://via.placeholder.com/468x110?text=thumbnail";
+    });
+  }
 }
