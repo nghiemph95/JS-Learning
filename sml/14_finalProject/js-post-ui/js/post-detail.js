463f2a4a17566febd3f816486eea5da4330a25b2
nghiemph95 <nghiemph95@gmail.com>
2022-10-04 23:42:49 +0700

    Updated: 04-10-2022 23:42:49

--- sml/14_finalProject/js-post-ui/js/post-detail.js
+++ sml/14_finalProject/js-post-ui/js/post-detail.js
@@ -1,9 +1,34 @@
+import postApi from "./api/postApi";
 
+// id="goToEditPageLink
+// id="postHeroImage
+// id="postDetailTitle
+// id="postDetailAuthor
+// id="postDetailTimeSpan
+// id="postDetailDescription
+
+function renderPostDetail(post) {
+  // render title
+  // render description
+  // render author
+  // render updateAt
+  // render hero image
+  // render edit page link
+}
 
 (async () => {
+  try {
     // lấy được ra post id từ URL
+    const searchParams = new URLSearchParams(window.location.search);
+    const postId = searchParams.get("id"); // id của bài post, từng thẻ li.div.post-item
+    if (!postId) return;
 
     // fetch dữ liệu chi tiết từ API
+    const post = await postApi.getById(postId); // dữ liệu từ server về bài post-detail
 
-    // render dữ liệu vừa fetch 
+    // render dữ liệu vừa fetch
+    renderPostDetail(post);
+  } catch (error) {
+    console.log("get by id have error", error);
+  }
 })();
