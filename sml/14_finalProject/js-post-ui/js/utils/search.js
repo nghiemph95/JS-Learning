dc0223bdb825ccda73c121e29e9d1af9db840e02
nghiemph95 <nghiemph95@gmail.com>
2022-10-04 15:19:02 +0700

    Updated: 04-10-2022 15:19:02

---
+++ sml/14_finalProject/js-post-ui/js/utils/search.js
@@ -0,0 +1,18 @@
+// hàm search
+export function initSearch() {
+  const searchInput = document.getElementById("searchInput");
+  if (!searchInput) return;
+
+  // set default value from query params
+  // title_like
+  const queryParams = new URLSearchParams(window.location.search);
+  if (queryParams.get("title_like")) {
+    searchInput.value = queryParams.get("title_like");
+  }
+
+  const debounceSearch = debounce(
+    (event) => handleFilterChange("title_like", event.target.value),
+    500
+  );
+  searchInput.addEventListener("input", debounceSearch);
+}
