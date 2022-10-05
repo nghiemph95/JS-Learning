a3139bd747473b6191f72e0b4d8b4a83f4d2e6ec
nghiemph95 <nghiemph95@gmail.com>
2022-10-05 22:11:40 +0700

    Updated: 05-10-2022 22:11:40

---
+++ sml/14_finalProject/js-post-ui/js/utils/lightbox.js
@@ -0,0 +1,7 @@
+export function registerLightbox() {
+  // handle click cho các image
+  // tìm các hình ảnh có cùng album/galary
+  // xác định hình click trong album đang nằm ở index nào
+  // hiển thị hình được chọn
+  // handle nút prev/next click chuyển hình
+}
