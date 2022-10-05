03e3e987f60e45e03b4a28ed48abd131bb6422a9
nghiemph95 <nghiemph95@gmail.com>
2022-10-05 23:52:40 +0700

    Updated: 05-10-2022 23:52:39

--- sml/14_finalProject/js-post-ui/js/utils/lightbox.js
+++ sml/14_finalProject/js-post-ui/js/utils/lightbox.js
@@ -1,4 +1,33 @@
-export function registerLightbox() {
+function showModel(modalElement) {
+  const modal = new window.bootstrap.Modal(modalElement);
+
+  if (modal) modal.show();
+}
+
+export function registerLightbox({
+  modalId,
+  imgSelector,
+  prevSelector,
+  nextSelector,
+}) {
+  const modalElement = document.getElementById(modalId);
+  if (!modalElement) return;
+
+  // lấy ra các selectors
+  const imageElement = modalElement.querySelector(imgSelector);
+  const prevButton = modalElement.querySelector(prevSelector);
+  const nextButton = modalElement.querySelector(nextSelector);
+
+  if (!imageElement || !prevButton || !nextButton) return;
+
+  // lightbox variable
+  let imgList = [];
+  let currentIndex = 0;
+
+  function showImageAtIndex(index) {
+    imageElement.src = imgList[index].src;
+  }
+
   // handle click cho các image -> áp dụng kỷ thuật event delegation
   document.addEventListener("click", (event) => {
     const { target } = event;
@@ -6,14 +35,23 @@ export function registerLightbox() {
 
     // tìm các hình ảnh có cùng album/galary
     // lấy các hình các data là album
-    let imgList = document.querySelectorAll(
+    imgList = document.querySelectorAll(
       `img[data-album="${target.dataset.album}"]`
     );
+
     // xác định hình click trong album đang nằm ở index nào
-    const index = [...imgList].findIndex((x) => x === target);
-    console.log("album click", { target, imgList, index });
+    currentIndex = [...imgList].findIndex((x) => x === target);
+
+    // hiển thị hình được chọn taij index
+    showImageAtIndex(currentIndex);
+
+    // hiển thị model
+    showModel(modalElement);
+    console.log("album click", { target, imgList, currentIndex });
   });
 
-  // hiển thị hình được chọn
   // handle nút prev/next click chuyển hình
+  prevButton.addEventListener("click", () => {});
+
+  nextButton.addEventListener("click", () => {});
 }
