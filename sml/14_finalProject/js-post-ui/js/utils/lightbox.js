bdf0a8fce433e7589eb26503195ca5c8b8c4fd76
nghiemph95 <nghiemph95@gmail.com>
2022-10-05 22:36:59 +0700

    Updated: 05-10-2022 22:36:59

--- sml/14_finalProject/js-post-ui/js/utils/lightbox.js
+++ sml/14_finalProject/js-post-ui/js/utils/lightbox.js
@@ -1,10 +1,19 @@
 export function registerLightbox() {
   // handle click cho các image -> áp dụng kỷ thuật event delegation
   document.addEventListener("click", (event) => {
-    console.log("click", event.target);
+    const { target } = event;
+    if (target.tagName !== "IMG" || !target.dataset.album) return;
+
+    // tìm các hình ảnh có cùng album/galary
+    // lấy các hình các data là album
+    let imgList = document.querySelectorAll(
+      `img[data-album="${target.dataset.album}"]`
+    );
+    // xác định hình click trong album đang nằm ở index nào
+    const index = [...imgList].findIndex((x) => x === target);
+    console.log("album click", { target, imgList, index });
   });
-  // tìm các hình ảnh có cùng album/galary
-  // xác định hình click trong album đang nằm ở index nào
+
   // hiển thị hình được chọn
   // handle nút prev/next click chuyển hình
 }
