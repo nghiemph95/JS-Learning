8487a00db3a74ae452e845abaec0ee22346e9500
nghiemph95 <nghiemph95@gmail.com>
2022-10-06 15:27:20 +0700

    Updated: 06-10-2022 15:27:20

--- sml/13_Node Package/06_webVitals.js
+++ sml/13_Node Package/06_webVitals.js
@@ -3,3 +3,24 @@
  * - FID: First Input Delay -> Interactivity: Khả năng tương tác
  * - CLS: Cumulative Layout Shift -> Visual Stability: TÍnh ổn định của trang
  */
+
+/**
+ * LCP: Largest Contentful Paint
+ * - Chỉ số đánh giá hiệu suất tải trang thông qua việc đo th gian
+ * hoàn tất phần tử lớn nhất trên trang web
+ * - LCP < 2.5s là tối ưu
+ */
+
+/**
+ * FID: First Input Delay
+ * - Chỉ số đo lường thời gian phản hồi lại tương tác đầu tiên
+ * của người dùng trên trang web.
+ * - FID < 100ms là tối ưu tốt
+ */
+
+/**
+ * CLS: Cumulative Layout Shift
+ * - Chỉ số đánh gái mức độ dịch chuyển bất ngờ của các phần tử 
+ * trên trang web mà người dùng không mong muốn
+ * - CLS: 0.1 đến 0.25s là tạm chấp nhận
+ */
