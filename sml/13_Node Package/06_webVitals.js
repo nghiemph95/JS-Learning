27bd8a951800ee688069c7a142542812caa7ad6b
nghiemph95 <nghiemph95@gmail.com>
2022-10-06 15:19:07 +0700

    Updated: 06-10-2022 15:19:07

---
+++ sml/13_Node Package/06_webVitals.js
@@ -0,0 +1,5 @@
+/** Core Web Vitals
+ * - LCP: Largest Contentful Paint -> Loading: Tốc độ tải trang
+ * - FID: First Input Delay -> Interactivity: Khả năng tương tác
+ * - CLS: Cumulative Layout Shift -> Visual Stability: TÍnh ổn định của trang
+ */
