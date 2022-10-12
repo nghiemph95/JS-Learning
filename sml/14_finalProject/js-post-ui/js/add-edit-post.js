28014dd91352885b51ea5db70aa288210c593196
nghiemph95 <nghiemph95@gmail.com>
2022-10-12 22:12:23 +0700

    Updated: 12-10-2022 22:12:23

--- sml/14_finalProject/js-post-ui/js/add-edit-post.js
+++ sml/14_finalProject/js-post-ui/js/add-edit-post.js
@@ -1,7 +1,22 @@
+import postApi from "./api/postApi";
+
 // MAIN
 (async () => {
   const searchParams = new URLSearchParams(window.location.search);
   const postId = searchParams.get("id");
 
-  console.log("add edit page", postId);
+  let defaultValues = {
+    title: "",
+    description: "",
+    author: "",
+    imageUrl: "",
+  };
+
+  if (postId) {
+    defaultValues = await postApi.getById(postId);
+  }
+
+  console.log("id:", postId);
+  console.log("mode: ", postId ? "edit" : "add");
+  console.log("defaultValues", defaultValues);
 })();
