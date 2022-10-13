0d8a88d16dc97af13f9b89c9aa8e700d40afca71
nghiemph95 <nghiemph95@gmail.com>
2022-10-13 11:25:11 +0700

    Updated: 13-10-2022 11:25:11

--- sml/14_finalProject/js-post-ui/js/utils/post-form.js
+++ sml/14_finalProject/js-post-ui/js/utils/post-form.js
@@ -1,9 +1,10 @@
-function setFormValues(formValues) {}
+function setFormValues(form, formValues) {}
 
 export function initPostForm({ formId, defaultValues, onSubmit }) {
   const form = document.getElementById(formId);
   if (!formId) return;
 
   // tạo form value (thằng cha cho giá trị gì thì set giá trị đó)
-  setFormValues(defaultValues);
+  console.log("form", form);
+  setFormValues(form, defaultValues);
 }
