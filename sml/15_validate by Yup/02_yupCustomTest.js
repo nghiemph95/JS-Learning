e58c51838e237e0fef85cf3c37628e1224b4ffc8
nghiemph95 <nghiemph95@gmail.com>
2022-10-15 21:01:17 +0700

    Updated: 15-10-2022 21:01:17

--- sml/15_validate by Yup/02_yupCustomTest.js
+++ sml/15_validate by Yup/02_yupCustomTest.js
@@ -2,7 +2,21 @@ import * as yup from "yup";
 
 (async () => {
   const schema = yup.object().shape({
-    title: yup.string().required("please enter title"),
+    title: yup
+      .string()
+      .required("please enter title")
+      .test(
+        "at-least-two-words",
+        "Please enter at least two words",
+        (value) => {
+          return (
+            value.split(" ").filter((x) => !!x && x.length >= 3).length > 2
+          );
+        }
+      )
+      .test("should-not-spam", 'Please not use "spam" in title', (value) => {
+        return !value.includes("spam");
+      }),
     age: yup.number().required("please enter age").min(10, "mis is ten"),
     email: yup.string().required().email("please enter a valide email"),
   });
