b3098ff02c3816e7ec21260ac2eaf74976e5844d
nghiemph95 <nghiemph95@gmail.com>
2022-10-16 22:55:01 +0700

    Updated: 16-10-2022 22:55:01

--- sml/14_finalProject/js-post-ui/js/utils/post-form.js
+++ sml/14_finalProject/js-post-ui/js/utils/post-form.js
@@ -1,4 +1,5 @@
 import { setFieldValue, setHeroImage, setTextContent } from "./common";
+import * as yup from "yup";
 
 function setFormValues(form, defaultValues) {
   setFieldValue(form, '[name="title"]', defaultValues?.title);
@@ -28,42 +29,87 @@ function getFormValues(form) {
 
 //hàm có nhiệm vụ trả về error message, nếu ko có error message thì trả về empty message
 //validation(4)
-function getTitleError(form) {
-  const titleElement = form.querySelector('[name="title"]');
-  if (!titleElement) return;
+// function getTitleError(form) {
+//   const titleElement = form.querySelector('[name="title"]');
+//   if (!titleElement) return;
 
-  //required
-  //kiểm tra xe đã nhập dữ liệu chưa (valueMissing = true tức là chưa) -validation(5)
-  if (titleElement.validity.valueMissing) {
-    return "Please enter title";
+//   //required
+//   //kiểm tra xe đã nhập dữ liệu chưa (valueMissing = true tức là chưa) -validation(5)
+//   if (titleElement.validity.valueMissing) {
+//     return "Please enter title";
+//   }
+
+//   // at least two words
+//   if (
+//     titleElement.value.split(" ").filter((x) => !!x && x.length >= 3).length < 2
+//   )
+//     return "Please enter at least two words of 3 characters";
+//   return "";
+// }
+
+//hàm check validation dựa vào Yup
+function getPostSchema() {
+  return yup.object().shape({
+    title: yup.string().required("Please enter title"),
+    author: yup
+      .string()
+      .required("Please enter author")
+      .test(
+        "at-least-two-words",
+        "Please enter at least two words",
+        (value) =>
+          value.split(" ").filter((x) => !!x && x.length >= 3).length >= 2
+      ),
+    description: yup.string(),
+  });
+}
+
+function setFieldError(form, name, error) {
+  const element = form.querySelector(`[name="${name}"]`);
+  if (element) {
+    // gắn error theo key(title, author, description,...) lên thẻ input
+    // validation(7): gắn "Please enter title vô cái setCustomValidity"
+    element.setCustomValidity(error);
+    //cập nhật cho thẻ div.invalid-feedback - validation(8)
+    setTextContent(element.parentElement, ".invalid-feedback", error);
   }
-
-  // at least two words
-  if (
-    titleElement.value.split(" ").filter((x) => !!x && x.length >= 3).length < 2
-  )
-    return "Please enter at least two words of 3 characters";
-  return "";
 }
 
 //hàm trả về true/false - validation(2)
-function validatePostForm(form, formValues) {
+async function validatePostForm(form, formValues) {
   //lấy thông tin error - validation(3)
-  const errors = {
-    title: getTitleError(form),
-    // title: getAuthorError(form),
-    //...
-  };
+  // const errors = {
+  //   title: getTitleError(form),
+  //   // title: getAuthorError(form),
+  //   //...
+  // };
 
   //gắn error mess lên dom - validation(6)
-  for (const key in errors) {
-    const element = form.querySelector(`[name="${key}"]`);
-    if (element) {
-      // gắn error theo key(title, author, description,...) lên thẻ input
-      // validation(7): gắn "Please enter title vô cái setCustomValidity"
-      element.setCustomValidity(errors[key]);
-      //cập nhật cho thẻ div.invalid-feedback - validation(8)
-      setTextContent(element.parentElement, ".invalid-feedback", errors[key]);
+  // for (const key in errors) {
+  //   const element = form.querySelector(`[name="${key}"]`);
+  //   if (element) {
+  //     // gắn error theo key(title, author, description,...) lên thẻ input
+  //     // validation(7): gắn "Please enter title vô cái setCustomValidity"
+  //     element.setCustomValidity(error);
+  //     //cập nhật cho thẻ div.invalid-feedback - validation(8)
+  //     setTextContent(element.parentElement, ".invalid-feedback", errors[key]);
+  //   }
+  // }
+
+  try {
+    //reset previous errors
+    ["title", "author"].forEach((name) => setFieldError(form, name, ""));
+
+    //validate với schema đã định nghĩa bên trên
+    const schema = getPostSchema();
+    await schema.validate(formValues, { abortEarly: false });
+  } catch (error) {
+    console.log(error.name); //error
+    console.log(error.inner); //mảng
+
+    for (const validationError of error.inner) {
+      const name = validationError.path;
+      setFieldError(form, name, validatePostForm.message);
     }
   }
 
