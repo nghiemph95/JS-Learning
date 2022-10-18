d1fe3eab12d31a4a97843da0ac68080888a8ca1e
nghiemph95 <nghiemph95@gmail.com>
2022-10-18 10:58:19 +0700

    Updated: 18-10-2022 10:58:19

--- sml/14_finalProject/js-post-ui/js/utils/post-form.js
+++ sml/14_finalProject/js-post-ui/js/utils/post-form.js
@@ -117,7 +117,6 @@ async function validatePostForm(form, formValues) {
 
         // set field error and mark as logged
         setFieldError(form, name, validationError.message);
-        // console.log(validationError.message);
         errorLog[name] = true;
       }
     }
