fbd68707435e3a3e5fc7d6cc5831f8c9249d815d
nghiemph95 <nghiemph95@gmail.com>
2022-12-07 14:42:46 +0700

    Updated: 07-12-2022 14:42:46

---
+++ sml/5_ES6 and JS Concept/9_valueType_referenceType.js
@@ -0,0 +1,11 @@
+/** Tham trị và tham chiếu
+ *
+ * Tham trị: Là lưu giá trị trực típ xún vùng nhớ
+ * Ví dụ: const a = 1000; => nó sẽ lưu trực típ xún vùng nhớ
+ *
+ * Tham chiếu: (Object, Array) -> lưu dưới dạng tham chiếu nhưng
+ * thay vì lưu giá trị thì nó sẽ lưu địa chỉ nơi lưu giữ giá trị
+ * Ví dụ: const a = {name: "nghiem"}
+ * thực chất a = 1E2F
+ * Kho chứa 1E2F là {name: "nghiem"}
+ */
