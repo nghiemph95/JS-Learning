c1d759d1bed2ebace39dfbf6b3338fdf07f59a3d
Nghiem Pham <nghiemph95@gmail.com>
2023-02-15 21:19:50 +0700

    Updated: 15-02-2023 21:19:50

--- sml/array/3_array_check/04-hasTruthy.js
+++ sml/array/3_array_check/04-hasTruthy.js
@@ -1,7 +1,8 @@
+/** Kiểm tra có giá trị truthy nào có trong mảng không */
 export function hasTruthy(arr) {
   if (arr.length === 0) return false;
 
   return arr.every((element) => element);
 }
 
-// console.log(hasTruthy([true, ""]));
+console.log(hasTruthy([true, 123]));
