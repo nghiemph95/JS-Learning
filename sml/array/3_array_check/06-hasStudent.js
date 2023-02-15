7575de88ac32c265bfcd410e0780c21309060137
Nghiem Pham <nghiemph95@gmail.com>
2023-02-15 21:23:07 +0700

    Updated: 15-02-2023 21:23:07

--- sml/array/3_array_check/06-hasStudent.js
+++ sml/array/3_array_check/06-hasStudent.js
@@ -1,3 +1,4 @@
+/** Kiểm tra có student có id nào trong mảng không */
 export function hasStudent(studentList, studentId) {
   for (let i = 0; i < studentList.length; i++) {
     for (const key in studentList[i]) {
