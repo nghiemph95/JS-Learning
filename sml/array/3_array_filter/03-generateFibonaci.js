50a6f111fe333e15899e6847fe509a13516c72de
Nghiem Pham <nghiemph95@gmail.com>
2023-02-16 23:02:31 +0700

    Updated: 16-02-2023 23:02:31

--- sml/array/3_array_filter/03-generateFibonaci.js
+++ sml/array/3_array_filter/03-generateFibonaci.js
@@ -1,18 +1,41 @@
-export function generateFibonaci(n) {
-  const fiboArray = [];
-  let firstElement = 0;
-  let secondElement = 1;
-  fiboArray.push(firstElement, secondElement);
+/** Dãy số fibonaci nhỏ hơn n */
+// export function generateFibonaci(n) {
+//   const fiboArray = [];
+//   let firstElement = 0;
+//   let secondElement = 1;
+//   fiboArray.push(firstElement, secondElement);
 
-  let thirdElement = firstElement + secondElement;
-  while (thirdElement < n) {
-    fiboArray.push(thirdElement);
-    firstElement = secondElement;
-    secondElement = thirdElement;
-    thirdElement = firstElement + secondElement;
+//   let thirdElement = firstElement + secondElement;
+//   while (thirdElement < n) {
+//     fiboArray.push(thirdElement);
+//     firstElement = secondElement;
+//     secondElement = thirdElement;
+//     thirdElement = firstElement + secondElement;
+//   }
+//   return fiboArray;
+// }
+
+export function generateFibonaci(n) {
+  const arr = [];
+
+  let firstEl = 0;
+  let secontEl = 1;
+  arr.push(firstEl, secontEl);
+
+  /** Calculation */
+  let thirdEl = firstEl + secontEl;
+  while (thirdEl < n) {
+    arr.push(thirdEl);
+    firstEl = secontEl;
+    secontEl = thirdEl;
+    thirdEl = firstEl + secontEl;
+    
   }
-  return fiboArray;
+
+  return arr;
 }
+
+console.log(generateFibonaci(100));
 
 export function generateFibonaciV2(n) {
   let fiboArray = [0, 1];
