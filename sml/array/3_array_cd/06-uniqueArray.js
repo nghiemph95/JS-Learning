9c609160b4a5522173acc68f9d83b25df03e65be
Nghiem Pham <nghiemph95@gmail.com>
2023-02-17 17:34:54 +0700

    Updated: 17-02-2023 17:34:54

--- sml/array/3_array_cd/06-uniqueArray.js
+++ sml/array/3_array_cd/06-uniqueArray.js
@@ -1,3 +1,4 @@
+/** Xoá các số trùng nhau nhưng giữ lại một số */
 export function uniqueArray(numberList) {
   if (!Array.isArray(numberList) || numberList.length === 0) return;
   //   First, convert an array of duplicates to a Set.
