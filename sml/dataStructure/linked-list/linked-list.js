e092e54626c3dc8d44b671c24b694e4b7a7a41e4
Nghiem Pham <nghiemph95@gmail.com>
2023-02-18 20:46:45 +0700

    Updated: 18-02-2023 20:46:45

--- sml/dataStructure/linked-list/linked-list.js
+++ sml/dataStructure/linked-list/linked-list.js
@@ -11,3 +11,13 @@
  * 2. Doubly (2 chiều)
  * 3. Circular
  */
+
+/** Việc thêm/xoá ở linked list rất đơn giản và tối ưu hơn Array */
+
+/** Khi nào cần dùng linked-list
+ * 1. Khi tạo 1 mảng dynamic size
+ * 2. Truy cập: Đi qua từng bước
+ * 3. Search: Linear (đi từng cái)
+ * 4. Add/remove: Đơn giản và không phải re-allocate items
+ * 5. Duyệt mảng (Traversal): Duyệt từ đầu đến cuối
+ */
