879b7e7004f4fb3e1fd1fc795b16f01f431f1af7
Nghiem Pham <nghiemph95@gmail.com>
2023-02-18 23:01:44 +0700

    Updated: 18-02-2023 23:01:44

--- sml/dataStructure/binary_tree/binaryTree.js
+++ sml/dataStructure/binary_tree/binaryTree.js
@@ -3,3 +3,13 @@
 /** Mỗi node bất kì có maximum là 2 node con
  * Node gồm: data (number, string, object), left, right (right, left có thể null)
  */
+
+/** node trên cùng đc gọi là root 
+ * giữa các node có liên kết gọi là edge
+ * 2 node cùng thuộc 1 node cha gọi là siblings (anh chị em)
+
+
+/** BST: Binary Search Tree có 1 lưu ý là 
+ * tất cả các node con bên trái đều nhỏ hơn node cha
+ * tất cả các node con bên phải đều lớn hơn node cha
+ */
