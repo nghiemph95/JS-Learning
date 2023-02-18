612f0396fb819986bf64f6d6a3327b0efaf3fa82
Nghiem Pham <nghiemph95@gmail.com>
2023-02-18 20:20:51 +0700

    Updated: 18-02-2023 20:20:51

---
+++ sml/dataStructure/linked-list/linked-list.js
@@ -0,0 +1,7 @@
+/** Linked List : Danh sách liên kết
+ * 1 phần tử gồm:
+ * - value (data)
+ * - pointer (tham chiếu tới phần tử tiếp theo)
+ * - phần tử đầu tiên: Head
+ * - phần tử cuối cùng: Tail
+ */
