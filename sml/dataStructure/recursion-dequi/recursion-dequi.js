da099ba3d9cff6d55468144e5632bf97bb360c2c
Nghiem Pham <nghiemph95@gmail.com>
2023-02-19 11:33:45 +0700

    Updated: 19-02-2023 11:33:45

--- sml/dataStructure/recursion-dequi/recursion-dequi.js
+++ sml/dataStructure/recursion-dequi/recursion-dequi.js
@@ -7,3 +7,11 @@
  * nếu không hàm sẽ được gọi lại liên tục
  * - Nếu để recursion gọi lại càng nhiều thì vô tình sẽ làm tăng bộ nhớ đệm
  */
+
+/** Có 6 loại recursion
+ * 1. Primitive recursion: Có thể chuyển sang dạng loop
+ * 2. Tail recursion: Là một dạng primitive recursion mà lời gọi recursive func nằm ở cuối hàm
+ * 3. Single recursion: Chỉ có một lời gọi recursive func
+ * 4. Multiple recursion: Có nhiều lời gọi recursive func
+ * 5. Mutual or Indirect recursion: Có ít nhất từ 2 function trở lên tạo nên recursion này: A -> B -> C -> A
+ */
