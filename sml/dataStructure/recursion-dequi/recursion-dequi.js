340a9b66f8e40020c19fc82285806b600dd701ea
Nghiem Pham <nghiemph95@gmail.com>
2023-02-19 11:29:44 +0700

    Updated: 19-02-2023 11:29:44

--- sml/dataStructure/recursion-dequi/recursion-dequi.js
+++ sml/dataStructure/recursion-dequi/recursion-dequi.js
@@ -1,3 +1,9 @@
 /** Đệ qui: Là quá trình mà function gọi lại chính nó một cách
  * gián tiếp hay trực tiếp
  */
+
+/** Lưu ý:
+ * - Khi làm việc phải có BASE CASE (điểm dừng, break point) để nó xác định điểm dừng,
+ * nếu không hàm sẽ được gọi lại liên tục
+ * - Nếu để recursion gọi lại càng nhiều thì vô tình sẽ làm tăng bộ nhớ đệm
+ */
