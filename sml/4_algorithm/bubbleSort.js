a8b1e7df062bd3844492d70dfa03bec004e983a7
Nghiem Pham <nghiemph95@gmail.com>
2023-02-22 15:57:14 +0700

    Updated: 22-02-2023 15:57:14

--- sml/4_algorithm/bubbleSort.js
+++ sml/4_algorithm/bubbleSort.js
@@ -1,7 +1,13 @@
+/** BubbleSort: So sánh 2 phần tử liền kề, nếu phần tử nào lớn hơn thì sẽ hoán đổi vị trí sao cho
+ * phần tử nhỏ hơn sẽ đứng trước phần tử lớn hơn
+ */
+
 function bubbleSort(numberList) {
   if (!Array.isArray(numberList) || numberList.length === 0) return [];
 
+  /** Chạy từ cuối mảng */
   for (let i = numberList.length - 1; i > 0; i--) {
+    /** Chạy từ đầu mảng */
     for (let j = 0; j < i; j++) {
       if (numberList[j] > numberList[j + 1]) {
         // swap bubble sort - recommend
