03d0b7e2bd29c6f91cf67fe8377bacc657430d8b
Nghiem Pham <nghiemph95@gmail.com>
2023-03-09 14:45:45 +0700

    Updated: 09-03-2023 14:45:45

--- sml/0_logic/04-findMostFrequentNumber.js
+++ sml/0_logic/04-findMostFrequentNumber.js
@@ -13,3 +13,33 @@ export function findMostFrequentNumber(numberList) {
 
   return count;
 }
+
+console.log(findMostFrequentNumber([1, 4, 4, 4, 5, 3]));
+
+nums.sort()
+       b=False
+       i=0
+       while i<(len(nums)-1):
+           if nums[i]==nums[i+1]:
+               b=True
+               return b
+           else:
+               b=False
+               i += 1
+       return b
+
+       const arr = [1,2,3,4,5,6,7,8,9];
+    
+const newArr = [];
+while(arr.length) newArr.push(arr.splice(0,3));
+
+var construct2DArray = function (original, m, n) {
+    if (m * n != original.length) {
+        return [];
+    }
+    const ans = [];
+    for (let i = 0; i < m * n; i += n) {
+        ans.push(original.slice(i, i + n));
+    }
+    return ans;
+};
