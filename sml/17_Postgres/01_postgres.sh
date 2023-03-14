37fc616ac6261cb80b92fcf7e2df17d939128ab7
Nghiem Pham <nghiemph95@gmail.com>
2023-03-14 15:14:07 +0700

    Updated: 14-03-2023 15:14:07

--- sml/17_Postgres/01_postgres.sh
+++ sml/17_Postgres/01_postgres.sh
@@ -29,3 +29,24 @@ ALTER TABLE
 // Điều này ko phải là điều đặc biệt. Tuy nhiên, điều gì sẽ xảy ra nếu 
 // add the same column to table cha giống table con: PostgreSQL sẽ hợp nhất
 // 2 column lại bởi vì chúng giống hệt nhau
+
+test=# ALTER TABLE t_data ADD COLUMN x int;
+NOTICE:  merging definition of column "x" for child "t_data_2" 
+ALTER TABLE
+
+/** Dropping Columns: Xoá cột */
+// Ví dụ: Khi xoá cột X ở table cha
+
+test=# ALTER TABLE t_data DROP COLUMN x;
+ALTER TABLE
+ 
+test=# \d t_data
+       Table "public.t_data"
+ Column |  Type   |      Modifiers
+--------+---------+-----------------------
+ id     | integer |
+ name   | text    |
+Number of child tables: 2 (Use \d+ to list them.)
+
+// Tuy nhiên cột X ở table: t_data_2 vẫn sẽ giữ nguyên bởi vì
+// Postgres ghi nhớ các cột được merged và đảm bảo cột vẫn giữ nguyên như cũ
