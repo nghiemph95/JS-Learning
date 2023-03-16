803883ddae59867616e79238ffe225c7497ef6ff
Nghiem Pham <nghiemph95@gmail.com>
2023-03-16 23:08:32 +0700

    Updated: 16-03-2023 23:08:32

--- sml/16_hackerRank/NodeJSBasicTest/02_paginationMiddleware.js
+++ sml/16_hackerRank/NodeJSBasicTest/02_paginationMiddleware.js
@@ -5,10 +5,12 @@ router.get("/", async (req, res) => {
   const { context } = req;
   const page = (context && context.page) || req.query.page;
   const limit = (context && context.limit) || req.query.limit;
+  
   const skip =
     (context && context.skip) || req.query.skip || (page - 1) * limit;
   const search = (context && context.searchTerm) || req.query.q;
   let results = [];
+
   if (search) {
     results = recipes.filter(({ name }) => name.match(search));
   } else {
