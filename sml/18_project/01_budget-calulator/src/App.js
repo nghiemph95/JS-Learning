c660e06b04a49e5dea3824d3f5c96411578852ff
Nghiem Pham <nghiemph95@gmail.com>
2023-03-21 21:20:50 +0700

    Updated: 21-03-2023 21:20:50

--- sml/18_project/01_budget-calulator/src/App.js
+++ sml/18_project/01_budget-calulator/src/App.js
@@ -3,12 +3,13 @@ import React from "react";
 import ExpenseList from "./components/ExpenseList";
 import ExpenseForm from "./components/ExpenseForm";
 import Alert from "./components/Alert";
-import { uuid } from "uuid/v4";
+
+import { v4 as uuidv4 } from "uuid";
 
 const initialExpenses = [
-  { id: uuid(), charge: "rent", amount: 1600 },
-  { id: uuid(), charge: "car payment", amount: 400 },
-  { id: uuid(), charge: "credit card bill", amount: 1200 },
+  { id: uuidv4(), charge: "rent", amount: 1600 },
+  { id: uuidv4(), charge: "car payment", amount: 400 },
+  { id: uuidv4(), charge: "credit card bill", amount: 1200 },
 ];
 
 console.log(initialExpenses);
