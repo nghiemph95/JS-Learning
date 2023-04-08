a73d79a49b14905bdf7a430bef863a58b1e7690c
Nghiem Pham <nghiemph95@gmail.com>
2023-04-08 16:02:46 +0700

    Updated: 08-04-2023 16:02:46

---
+++ sml/19_mongoExpress/src/index.js
@@ -0,0 +1,39 @@
+// const express = require("express");
+// const path = require("path");
+// const morgan = require("morgan");
+// const handlebars = require("express-handlebars");
+import express from 'express';
+import path from 'path';
+import morgan from 'morgan';
+import { engine } from 'express-handlebars';
+import { fileURLToPath } from 'url';
+
+//App init
+const app = express();
+const port = 3000;
+
+// Create __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// get Image
+app.use(express.static(path.join(__dirname, 'public')));
+
+//HTTP logger
+app.use(morgan('combined'));
+
+//Template engine
+app.engine(
+  'hbs',
+  engine({
+    extname: '.hbs',
+  }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+// Route
+app.get('/', (req, res) => res.render('home'));
+app.get('/news', (req, res) => res.render('news'));
+
+app.listen(port, () => console.log(`Example app listenning at http://localhost:${port}`));
