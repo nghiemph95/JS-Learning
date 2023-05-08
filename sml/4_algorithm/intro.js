f9fb7854925d0ecdfdfa49dc82d89b4d5ce9ec44
Nghiem Pham <nghiemph95@gmail.com>
2023-05-08 22:47:50 +0700

    Updated: 08-05-2023 22:47:50

--- sml/4_algorithm/intro.js
+++ sml/4_algorithm/intro.js
@@ -4,3 +4,28 @@
  */
 
 console.log([1, 2, 3].includes(1));
+
+
+const compareAssignedListChangedV1 = (newList, oldList) => {
+  const elementsInOld = new Set(oldList);
+  const allElementInOld = newList.every((element) =>
+    elementsInOld.has(element)
+  );
+  return allElementInOld;
+};
+
+const compareAssignedListChanged = (newList, oldList) => {
+  const elementsInOld = new Set(oldList);
+  const allElementsInOld = newList.every((element) =>
+    elementsInOld.has(element)
+  );
+  const allElementsInNew = oldList.every((element) =>
+    new Set(newList).has(element)
+  );
+  return (
+    allElementsInOld && allElementsInNew && newList.length === oldList.length
+  );
+};
+
+
+console.log(compareAssignedListChanged([3, 2], [1, 2, 3]));
