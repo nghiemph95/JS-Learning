02b7e7f35aa96b4658d1d0cc4f52166ce034cace
Nghiem Pham <nghiemph95@gmail.com>
2023-07-28 09:27:51 +0700

    Updated: 28-07-2023 09:27:51

--- sml/4_algorithm/intro.js
+++ sml/4_algorithm/intro.js
@@ -5,7 +5,6 @@
 
 console.log([1, 2, 3].includes(1));
 
-
 const compareAssignedListChangedV1 = (newList, oldList) => {
   const elementsInOld = new Set(oldList);
   const allElementInOld = newList.every((element) =>
@@ -58,5 +57,161 @@ const compareAssignedListChanged = (newList, oldList) => {
   return false;
 };
 
-
 console.log(compareAssignedListChanged([], [1, 2, 3]));
+
+const a = {
+  position: 0,
+  size: 0,
+  rotaion: 0,
+  scale: 0,
+  order: 0,
+  placementType: "",
+  tel: "",
+  a1: { label: "bbb", value: "bbb" },
+  a2: "xxx",
+  a3: { label: "yyy", value: "yyy" },
+};
+
+const b = [
+  {
+    key: "a1",
+    attribute: '["aaa","ddd","eee"]',
+    dataType: "dropdown",
+  },
+  {
+    key: "a2",
+    attribute: "",
+    dataType: "text",
+  },
+  {
+    key: "a3",
+    attribute: '["xyz","yyy","otp"]',
+    dataType: "dropdown",
+  },
+];
+
+const result = b.every((element) => {
+  if (
+    element.dataType === "dropdown" &&
+    a[element.key]?.value &&
+    element.attribute
+  ) {
+    const attributeArray = JSON.parse(element.attribute);
+    return attributeArray.includes(a[element.key].value);
+  }
+  return true; // Skip non-dropdown elements
+});
+
+console.log(result);
+
+// const result = b.every((element) => {
+//   if (a[element.key]) {
+//     if (a[element.key].value && element.attribute) {
+//       const attributeArray = JSON.parse(element.attribute);
+//       return attributeArray.includes(a[element.key].value);
+//     }
+//   }
+//   return false;
+// });
+
+console.log(result);
+
+// const result = b.map((element) => {
+//   if (a[element.key]) {
+//     if (a[element.key].value && element.attribute) {
+//       const attributeArray = JSON.parse(element.attribute);
+//       return attributeArray.includes(a[element.key].value);
+//     }
+//   }
+//   return false;
+// });
+
+// let result = false;
+
+// b.forEach((element) => {
+//   if (element.key === "a1" && a[element.key]) {
+//     const attributeArray = JSON.parse(element.attribute);
+//     if (attributeArray.includes(a[element.key].value)) {
+//       result = true;
+//     }
+//   }
+// });
+
+// console.log(result);
+
+b.forEach((element) => {
+  if (a[element.key]) {
+    if (a[element.key].value && element.attribute) {
+      const attributeArray = JSON.parse(element.attribute);
+      if (attributeArray.includes(a[element.key].value)) {
+        console.log(`Value for key ${element.key} is included in attribute.`);
+      } else {
+        console.log(
+          `Value for key ${element.key} is not included in attribute.`
+        );
+      }
+    } else {
+      console.log(
+        `Value for key ${element.key} is not defined or attribute is empty.`
+      );
+    }
+  } else {
+    console.log(`Key ${element.key} does not exist in 'a'.`);
+  }
+});
+
+const result = attributes.every((element: any) => {
+  if (
+    element.dataType === AttributeDataType.DROPDOWN &&
+    containerInUseAttributes.attributes[element.key]?.value &&
+    element.attributeValues
+  ) {
+    const attributeArray = JSON.parse(element.attributeValues);
+    return attributeArray.includes(
+      containerInUseAttributes.attributes[element.key].value
+    );
+  }
+  return true;
+});
+
+const containerInUseAttributes = await this._container.findOne({
+  where: {
+    tenantId: id,
+  },
+  select: ["attributes"],
+});
+if (containerInUseAttributes) {
+  if (
+    !checkAttributesInUseInContainer(
+      attributes,
+      containerInUseAttributes.attributes
+    )
+  ) {
+    throw TenantError.attributesInUseInContainer();
+  }
+}
+
+
+const attributeValidator = (attributes: any) => {
+  const inValidKey = (str: string) => {
+    return !/^[a-z0-9_]*$/.test(str);
+  };
+  const validAttrs =
+    attributes.filter((attribute: any) => {
+      return (
+        (attribute.key.length > 0 && attribute.label.length <= 0) ||
+        (attribute.key.length <= 0 && attribute.label.length > 0) ||
+        inValidKey(attribute.key as string) ||
+        !attribute.key ||
+        !attribute.label ||
+        (attribute.dataType === AttributeDataType.DROPDOWN &&
+          attribute.attributeValues.length <= 0) ||
+        (attribute.dataType === AttributeDataType.TEXT &&
+          attribute.attributeValues.length > 0) ||
+        (attribute.attributeValues.length > 0 &&
+          JSON.parse(attribute.attributeValues) > 20)
+      );
+    }).length <= 0;
+  if (!validAttrs || attributes?.length > 20)
+    throw CommonError.commonSomethingWentWrongError();
+};
---
+++ sml/4_algorithm/test.ts
@@ -0,0 +1,44 @@
+const attributeValidator = (attributes: any) => {
+  const inValidKey = (str: string) => {
+    return !/^[a-z0-9_]*$/.test(str);
+  };
+
+  const hasInvalidKey = attributes.some((attribute: any) => {
+    return !attribute.key || inValidKey(attribute.key as string);
+  });
+
+  const hasMissingKeyOrLabel = attributes.some((attribute: any) => {
+    return (
+      (attribute.key.length === 0 && attribute.label.length > 0) ||
+      (attribute.key.length > 0 && attribute.label.length === 0)
+    );
+  });
+
+  const hasInvalidDataType = attributes.some((attribute: any) => {
+    return (
+      (attribute.dataType === AttributeDataType.DROPDOWN &&
+        attribute.attributeValues.length === 0) ||
+      (attribute.dataType === AttributeDataType.TEXT &&
+        attribute.attributeValues.length > 0)
+    );
+  });
+
+  const hasInvalidAttributeValues = attributes.some((attribute: any) => {
+    return (
+      attribute.attributeValues.length > 0 &&
+      JSON.parse(attribute.attributeValues) > 20
+    );
+  });
+
+  const hasTooManyAttributes = attributes.length > 20;
+
+  if (
+    hasInvalidKey ||
+    hasMissingKeyOrLabel ||
+    hasInvalidDataType ||
+    hasInvalidAttributeValues ||
+    hasTooManyAttributes
+  ) {
+    throw CommonError.commonSomethingWentWrongError();
+  }
+};
